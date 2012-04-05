579dc7d5d013f85fd34ff99138204b9bf5091684
Matt Pass <matt@mattpass.com>
2012-04-05 07:58:09 +0100

    Version no update to v0.6.1

--- lib/settings.php
+++ lib/settings.php
@@ -1,5 +1,5 @@
 <?php
-$versionNo		= "v 0.6.0";
+$versionNo		= "v 0.6.1";
 $codeMirrorDir		= "CodeMirror-2.23";
 $cMThisVer		= 2.23;
 $testcMVersion		= false; // test if we're using the latest CodeMirror version
