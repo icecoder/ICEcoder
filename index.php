8019f66a78f832fdb2227a1b7b49f4762554c3be
Matt Pass <matt@mattpass.com>
2012-04-20 08:01:13 +0100

    Default = icecoder fix
    If you choose the default theme, we need to identify that as 'iceocder'

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if ($testcMVersion) {
 <link rel="stylesheet" type="text/css" href="lib/coder.css">
 <script>
 shortURLStarts = "<?php echo $shortURLStarts;?>";
-theme = "<?php echo $theme;?>";
+theme = "<?php if ($theme=="default") {echo 'icecoder';} else {echo $theme;};?>";
 <?
 $docRoot = str_replace("\\","/",$_SERVER['DOCUMENT_ROOT']);
 if (strrpos($docRoot,"/")==strlen($docRoot)-1) {$docRoot = substr($docRoot,0,strlen($docRoot)-1);};
