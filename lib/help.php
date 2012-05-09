57e820501aab5525b8e5ee56cdab0cb0cd62db4a
Matt Pass <matt@mattpass.com>
2012-05-09 17:08:14 +0100

    Help popup added
    New help popup screen, accessed by clicking ICEcoder logo
    Shows shortcut keys for now

---
+++ lib/help.php
@@ -0,0 +1,38 @@
+<?php include("settings.php");?>
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>ICE Coder - <?php echo $versionNo;?> :: Help & Shortcuts</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="help.css">
+</head>
+
+<body class="help">
+
+<h1 id="title">help</h1>
+
+<h2>In file manager</h2>
+<span class="key">Delete</span> <span class="shortcut">Delete file</span><br><br>
+
+<h2>Within document</h2>
+<span class="key">CTRL+I</span> <span class="shortcut">Search selected text</span><br>
+<span class="key">Esc</span> <span class="shortcut">Comment/uncomment text</span><br>
+<span class="key">Tab</span> <span class="shortcut">Insert tab/indent selected text</span><br><br>
+
+<h2>Anywhere</h2>
+<span class="key">CTRL+F</span> <span class="shortcut">Find</span><br>
+<span class="key">CTRL+G</span> <span class="shortcut">Go to line</span><br>
+<span class="key">CTRL+S</span> <span class="shortcut">Save</span><br>
+<span class="key">CTRL+Shift+S</span> <span class="shortcut">Save as...</span><br>
+<span class="key">CTRL+Enter</span> <span class="shortcut">View webpage</span><br>
+<span class="key">CTRL+S+Enter</span> <span class="shortcut">Save & create sticky tab</span><br>
+<span class="key">Esc</span> <span class="shortcut">Cancel tasks</span><br><br>
+
+<h2>Clicking logo</h2>
+<span class="key">Left click</span> <span class="shortcut">Help</span><br>
+<span class="key">Right click</span> <span class="shortcut">Settings</span><br>
+
+</body>
+
+</html>
