d1df67338d58ab66fdd0eaef33966991cbdb342d
Matt Pass <matt@mattpass.com>
2012-05-28 18:53:40 +0100

    ICEcoder & user settings now stored in new file
    Settings moved from settings.php to this new config.php file
    This seperates processing from data storage as ICEcoder evolves

---
+++ lib/config.php
@@ -0,0 +1,23 @@
+<?php
+$versionNo		= "v 0.6.8";
+$codeMirrorDir		= "CodeMirror-2.25";
+$cMThisVer		= 2.25;
+$tabsIndent		= true;
+$testcMVersion		= false;
+$openLastFiles		= true;
+$findFilesExclude	= array("_coder");
+$codeAssist		= true;
+$visibleTabs		= false;
+$lockedNav		= true;
+$accountPassword	= "";
+$restrictedFiles	= array("wp-",".php",".rb",".sql");
+$bannedFiles		= array("_coder","wp-",".exe");
+$allowedIPs		= array("*");
+$plugins		= array(
+			array("Database Admin","images/database.png","margin-top: 3px","plugins/adminer/adminer-3.3.3-mysql-en.php","_blank",""),
+			array("Batch Image Processor","images/images.png","margin-top: 5px","http://birme.net","_blank",""),
+			array("Backup","images/backup-open-files.png","margin-top: 3px","plugins/backupOpenFiles/index.php","fileControl:<b>Zipping Open Files</b>","10")
+			);
+$theme			= "default";
+$lastOpenedFiles	= "";
+?>
