9cbb9f75291482d3c4dd8d37a21f6a187fdae3f1
Matt Pass <matt@mattpass.com>
2012-06-23 17:20:40 +0100

    Zip It! new URL format
    Don't need index.php, so removed that
    Added extra query string param to list files that should be excluded
    from zip
    Value of this param is media files (as per list we want to exclude
    searching on)
    Means you don't end up with long processes and potentially big zips

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ $allowedIPs		= array("*");
 $plugins		= array(
 			array("Database Admin","images/database.png","margin-top: 3px","plugins/adminer/adminer-3.3.3-mysql-en.php","_blank",""),
 			array("Batch Image Processor","images/images.png","margin-top: 5px","http://birme.net","_blank",""),
-			array("Zip It!","images/zip-it.png","margin-top: 3px","plugins/zip-it/index.php?zip=|","fileControl:<b>Zipping Open Files</b>","10")
+			array("Zip It!","images/zip-it.png","margin-top: 3px","plugins/zip-it/?zip=|&exclude=.doc,.gif,.jpg,.jpeg,.pdf,.png,.swf,.xml,.zip","fileControl:<b>Zipping Open Files</b>","10")
 			);
 $theme			= "default";
 $tabWidth		= 4;
