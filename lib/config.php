1fa86272d6cb9807490e534a2efdc8e67ff5962b
Matt Pass <matt@mattpass.com>
2012-07-27 11:26:14 +0100

    version nos moved, root change & ZipIt removed
    versionNo, codeMirrorDir & cMThisVer now moved to settings.php
    These 3 settings are prefixed to array after initiated
    This means this file doesn't need to get updated each time there is a
    version no change. This in turn means that user settings aren't
    overwritten each time a release made and pulled from Github
    root setting is now the relative path from the doc root only, not
    prefixed by it (ie, full path)
    ZipIt plugin removed until I figure out the issues with it after
    changing settings via settings screen

--- lib/config.php
+++ lib/config.php
@@ -1,9 +1,6 @@
 <?php
 $ICEcoder = array(
-"versionNo"		=> "v 0.7.9",
-"codeMirrorDir"		=> "CodeMirror-2.32",
-"cMThisVer"		=> 2.32,
-"root"			=> $_SERVER['DOCUMENT_ROOT']."",
+"root"			=> "",
 "tabsIndent"		=> true,
 "checkUpdates"		=> false,
 "openLastFiles"		=> true,
@@ -18,7 +15,6 @@ $ICEcoder = array(
 "plugins"		=> array(
 			array("Database Admin","images/database.png","margin-top: 3px","plugins/adminer/adminer-3.3.3-mysql-en.php","_blank",""),
 			array("Batch Image Processor","images/images.png","margin-top: 5px","http://birme.net","_blank",""),
-			array("Zip It!","images/zip-it.png","margin-top: 3px","plugins/zip-it/?zip=|&exclude=.doc,.gif,.jpg,.jpeg,.pdf,.png,.swf,.xml,.zip","fileControl:<b>Zipping Files</b>","30")
 			),
 "theme"			=> "default",
 "tabWidth"		=> 4,
