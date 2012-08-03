b485e1ce4eb7ebbb88451ec641c43a5ca2dc7d82
Matt Pass <matt@mattpass.com>
2012-08-03 13:06:36 +0100

    Icons moved/renamed and ICErepo plugin added
    Adminer & Zip-It! have their icons renamed to icon.png now, URLs to them
    have changed.
    Also renamed the title for Adminer to this
    New plugin icon added for ICErepo as a placeholder
    Minor CSS adjustments

--- lib/config.php
+++ lib/config.php
@@ -13,8 +13,9 @@ $ICEcoder = array(
 "bannedFiles"		=> array("_coder","wp-",".exe"),
 "allowedIPs"		=> array("*"),
 "plugins"		=> array(
-			array("Database Admin","images/database.png","margin-top: 3px","plugins/adminer/adminer-3.4.0-mysql-en.php","_blank",""),
-			array("Zip It!","images/zip-it.png","margin-top: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
+			array("Adminer","plugins/adminer/icon.png","margin-top: 3px","plugins/adminer/adminer-3.4.0-mysql-en.php","_blank",""),
+			array("Zip It!","plugins/zip-it/icon.png","margin-top: 3px; margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30"),
+			array("ICErepo","plugins/ice-repo/icon.png","margin-top: 3px","plugins/ice-repo","_blank","")
 			),
 "theme"			=> "default",
 "tabWidth"		=> 4,
