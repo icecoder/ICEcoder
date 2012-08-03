5de12ce6513862e7c93ac055ca673b943e236b9a
Matt Pass <matt@mattpass.com>
2012-08-03 13:04:38 +0100

    ICErepo plugin placeholder added
    Some basic info and link to ICErepo project on Github
    Icon also added for this plugin

---
+++ plugins/ice-repo/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<title>Github plugin coming soon!</title>
+</head>
+
+<body style="font-family: arial, helvetica, swiss, verdana">
+
+<b>ICErepo - Plugin coming soon!</b>
+<br><br>
+
+I'm creating a Github plugin right now which will allow you to sync website code with Github repos.
+<br><br>
+Full details and dev at:<br>
+<a href="http://github.com/mattpass/ICErepo">http://github.com/mattpass/ICErepo</a>
+<br><br>
+<b>Expected launch date:</b><br>
+Wed 22nd Aug
+
+</body>
+
+</html>
