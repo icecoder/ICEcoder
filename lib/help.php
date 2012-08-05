d51f050d6e260d8ec3326bfe17a65ae462012f6d
Matt Pass <matt@mattpass.com>
2012-08-05 14:18:07 +0100

    6 new shortcuts added
    Shortcut info added for move line up/down, new/previous tab, new tab &
    close current tab
    Minor text adjustment to heading

--- lib/help.php
+++ lib/help.php
@@ -16,11 +16,17 @@
 <span class="key">Delete</span> <span class="shortcut">Delete file</span><br><br>
 
 <h2>Within document</h2>
+<span class="key">CTRL <span class="plus">+</span> &uarr;</span> <span class="shortcut">Move line up</span><br>
+<span class="key">CTRL <span class="plus">+</span> &darr;</span> <span class="shortcut">Move line down</span><br>
 <span class="key">CTRL <span class="plus">+</span> I</span> <span class="shortcut">Search selected text</span><br>
 <span class="key">Esc</span> <span class="shortcut">Comment / uncomment text</span><br>
 <span class="key">Tab</span> <span class="shortcut">Insert tab / indent selected text</span><br><br>
 
 <h2>Anywhere</h2>
+<span class="key">CTRL <span class="plus">+</span> &rarr;</span> <span class="shortcut">Next tab</span><br>
+<span class="key">CTRL <span class="plus">+</span> &larr;</span> <span class="shortcut">Previous tab</span><br>
+<span class="key">CTRL <span class="plus">+</span> +</span> <span class="shortcut">New tab</span><br>
+<span class="key">CTRL <span class="plus">+</span> -</span> <span class="shortcut">Close current tab</span><br>
 <span class="key">CTRL <span class="plus">+</span> F</span> <span class="shortcut">Find</span><br>
 <span class="key">CTRL <span class="plus">+</span> G</span> <span class="shortcut">Go to line</span><br>
 <span class="key">CTRL <span class="plus">+</span> S</span> <span class="shortcut">Save</span><br>
@@ -29,7 +35,7 @@
 <span class="key">CTRL <span class="plus">+</span> S <span class="plus">+</span> Enter</span> <span class="shortcut">Save & create sticky tab</span><br>
 <span class="key">Esc</span> <span class="shortcut">Cancel tasks</span><br><br>
 
-<h2>Clicking logo</h2>
+<h2>Logo</h2>
 <span class="key">Left click</span> <span class="shortcut">Help</span><br>
 <span class="key">Right click</span> <span class="shortcut">Settings</span><br>
 
