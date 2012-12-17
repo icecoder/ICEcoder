0eaef469f97b83d6494fd3296847a2d1c8b314f1
Matt Pass <matt@mattpass.com>
2012-12-17 08:59:59 +0000

    Tabs change color hover

--- index.php
+++ index.php
@@ -124,7 +124,7 @@ if (file_exists(dirname(__FILE__)."/plugins/jshint/jshint.js")) {
 	<div id="tabsBar" class="tabsBar" onContextMenu="return false">
 		<?php
 		for ($i=1;$i<=100;$i++) {
-			echo '<div id="tab'.$i.'" class="tab" draggable="true" onClick="ICEcoder.canSwitchTabs ? ICEcoder.switchTab('.$i.') : ICEcoder.canSwitchTabs=true"></div>';
+			echo '<div id="tab'.$i.'" class="tab" draggable="true" onClick="ICEcoder.canSwitchTabs ? ICEcoder.switchTab('.$i.') : ICEcoder.canSwitchTabs=true; thisColor=\'#000\'" onMouseOver="thisColor=this.style.color;this.style.color=\'#000\'" onMouseOut="this.style.color=thisColor"></div>';
 		}
 		?><div class="newTab" onClick="ICEcoder.newTab()"><img src="images/nav-new.png"></div>
 	</div>
