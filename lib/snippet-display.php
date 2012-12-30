f73dee1103548d040003650d7ac9b94c125b8703
Matt Pass <matt@mattpass.com>
2012-12-30 17:11:50 +0000

    Snippets display moved & now for info
    Snippets display file now moved to lib folder
    Functionality removed from this file and now just used for info purposes
    Tidied up look and provided table of uses, plus note on Emmet's tab key

---
+++ lib/snippet-display.php
@@ -0,0 +1,28 @@
+<?php include("lib/settings.php");?>
+<!DOCTYPE html>
+
+<html style="margin: 0">
+<head>
+<title>ICEcoder v <?php echo $ICEcoder["versionNo"];?> snippet display</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="robots" content="noindex, nofollow">
+<style type="text/css">
+body {font-family: arial; font-size: 10px; background: #ccc}
+</style>
+</head>
+
+<body style="margin: 5px">
+
+<table border="0" cellpadding="0" cellspacing="0" width="500">
+<tr valign="top"><td style="padding-bottom: 5px; font-size: 11px"><b>abbreviation plus CTRL+space</td><td style="font-size: 11px"><b>produces</b></td></tr>
+<tr valign="top"><td>f name</td><td style="padding-bottom: 5px">function name() {...}</td></tr>
+<tr valign="top"><td>if</td><td style="padding-bottom: 5px">if () {...}</td></tr>
+<tr valign="top"><td>for</td><td style="padding-bottom: 5px">for (var i=0; i<; i++) {...}</td></tr>
+</table>
+<br><br>
+Tip: If you have Emmet installed, also try tab key after your abbreviation
+</script>
+
+</body>
+
+</html>
