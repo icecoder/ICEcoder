f362a83cfa5dad7502a2e9d99a6f41409ffbca0f
Matt Pass <matt@mattpass.com>
2013-02-26 16:30:08 +0000

    Add 8 tag wrapper shortcuts & 2 column format
    8 new tag wrapper shortcuts added and moving from 1 column to 2 because
    of this extra info

--- lib/help.php
+++ lib/help.php
@@ -15,36 +15,48 @@
 
 <a href="javascript:top.ICEcoder.showManual(<?php echo $ICEcoder["versionNo"];?>)" style="position: absolute; top: 26px; right: 20px"><div style="padding: 10px; background: #2187e7; color: #fff; font-size: 18px">ICEcoder manual</div></a>
 
-<h2>In file manager</h2>
-<span class="key">Left click</span> <span class="shortcut">Select file / folder</span><br>
-<span class="key">Double click</span> <span class="shortcut">Open file</span><br>
-<span class="key">CTRL <span class="plus">+</span> click</span> <span class="shortcut">Multiple select</span><br>
-<span class="key">Right click</span> <span class="shortcut">Options for selected</span><br>
-<span class="key">Delete</span> <span class="shortcut">Delete selected</span><br><br>
+<div style="display: inline-block; width: 365px; margin-right: 20px">
+	<h2>Within file manager</h2>
+	<span class="key">Left click</span> <span class="shortcut">Select file / folder</span><br>
+	<span class="key">Double click</span> <span class="shortcut">Open file</span><br>
+	<span class="key">CTRL <span class="plus">+</span> click</span> <span class="shortcut">Multiple select</span><br>
+	<span class="key">Right click</span> <span class="shortcut">Options for selected</span><br>
+	<span class="key">Delete</span> <span class="shortcut">Delete selected</span><br><br><br>
 
-<h2>Within document</h2>
-<span class="key">CTRL <span class="plus">+</span> &uarr;</span> <span class="shortcut">Move line up</span><br>
-<span class="key">CTRL <span class="plus">+</span> &darr;</span> <span class="shortcut">Move line down</span><br>
-<span class="key">CTRL <span class="plus">+</span> I</span> <span class="shortcut">Search selected text</span><br>
-<span class="key">Esc</span> <span class="shortcut">Comment / uncomment text</span><br>
-<span class="key">Tab</span> <span class="shortcut">Insert tab / indent selected text</span><br><br>
+	<h2>Within document</h2>
+	<span class="key">CTRL <span class="plus">+</span> &uarr;</span> <span class="shortcut">Move line up</span><br>
+	<span class="key">CTRL <span class="plus">+</span> &darr;</span> <span class="shortcut">Move line down</span><br>
+	<span class="key">CTRL <span class="plus">+</span> I</span> <span class="shortcut">Search selected text</span><br>
+	<span class="key">Esc</span> <span class="shortcut">Comment / uncomment text</span><br>
+	<span class="key">Tab</span> <span class="shortcut">Insert tab / indent selected text</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> d</span> <span class="shortcut">Wrap selected in &lt;div&gt;</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> s</span> <span class="shortcut">Wrap selected in &lt;span&gt;</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> p</span> <span class="shortcut">Wrap selected in &lt;p&gt;</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> a</span> <span class="shortcut">Wrap selected in &lt;a&gt;</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> b</span> <span class="shortcut">Wrap selected in &lt;b&gt;</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> i</span> <span class="shortcut">Wrap selected in &lt;i&gt;</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> g</span> <span class="shortcut">Wrap selected in &lt;strong&gt;</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> e</span> <span class="shortcut">Wrap selected in &lt;em&gt;</span><br>
+</div>
 
-<h2>Anywhere</h2>
-<span class="key">CTRL <span class="plus">+</span> &rarr;</span> <span class="shortcut">Next tab</span><br>
-<span class="key">CTRL <span class="plus">+</span> &larr;</span> <span class="shortcut">Previous tab</span><br>
-<span class="key">CTRL <span class="plus">+</span> +</span> <span class="shortcut">New tab</span><br>
-<span class="key">CTRL <span class="plus">+</span> -</span> <span class="shortcut">Close current tab</span><br>
-<span class="key">CTRL <span class="plus">+</span> F</span> <span class="shortcut">Find</span><br>
-<span class="key">CTRL <span class="plus">+</span> G</span> <span class="shortcut">Go to line</span><br>
-<span class="key">CTRL <span class="plus">+</span> S</span> <span class="shortcut">Save</span><br>
-<span class="key">CTRL <span class="plus">+</span> Shift <span class="plus">+</span> S</span> <span class="shortcut">Save as...</span><br>
-<span class="key">CTRL <span class="plus">+</span> Enter</span> <span class="shortcut">View webpage</span><br>
-<span class="key">CTRL <span class="plus">+</span> S <span class="plus">+</span> Enter</span> <span class="shortcut">Save & create sticky tab</span><br>
-<span class="key">Esc</span> <span class="shortcut">Cancel tasks</span><br><br>
+<div style="display: inline-block; width: 365px">
+	<h2>Anywhere</h2>
+	<span class="key">CTRL <span class="plus">+</span> &rarr;</span> <span class="shortcut">Next tab</span><br>
+	<span class="key">CTRL <span class="plus">+</span> &larr;</span> <span class="shortcut">Previous tab</span><br>
+	<span class="key">CTRL <span class="plus">+</span> +</span> <span class="shortcut">New tab</span><br>
+	<span class="key">CTRL <span class="plus">+</span> -</span> <span class="shortcut">Close current tab</span><br>
+	<span class="key">CTRL <span class="plus">+</span> F</span> <span class="shortcut">Find</span><br>
+	<span class="key">CTRL <span class="plus">+</span> G</span> <span class="shortcut">Go to line</span><br>
+	<span class="key">CTRL <span class="plus">+</span> S</span> <span class="shortcut">Save</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Shift <span class="plus">+</span> S</span> <span class="shortcut">Save as...</span><br>
+	<span class="key">CTRL <span class="plus">+</span> Enter</span> <span class="shortcut">View webpage</span><br>
+	<span class="key">CTRL <span class="plus">+</span> S <span class="plus">+</span> Enter</span> <span class="shortcut">Save & create sticky tab</span><br>
+	<span class="key">Esc</span> <span class="shortcut">Cancel tasks</span><br><br><br>
 
-<h2>Logo</h2>
-<span class="key">Left click</span> <span class="shortcut">Help</span><br>
-<span class="key">Right click</span> <span class="shortcut">Settings</span><br>
+	<h2>On logo</b><h2>
+	<span class="key">Left click</span> <span class="shortcut">Help</span><br>
+	<span class="key">Right click</span> <span class="shortcut">Settings</span><br>
+</div>
 
 </body>
 
