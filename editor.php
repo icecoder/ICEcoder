7abadcc6c37e9fccad06fd3d530830dc037ba06e
Matt Pass <matt@mattpass.com>
2013-03-02 11:23:53 +0000

    Note added on Emmet using Tabs now

--- editor.php
+++ editor.php
@@ -104,7 +104,7 @@ $activeLineBG = array_search($ICEcoder["theme"],array("eclipse","elegant","neat"
 
 <script>
 CodeMirror.keyMap.ICEcoder = {
-	//"Tab": function(cm) {CodeMirror.commands[top.ICEcoder.tabsIndent ? "defaultTab" : "insertTab"](cm);},
+	// "Tab": "defaultTab", **Now used by Emmet**
 	"Shift-Tab": "indentLess",
 	"Ctrl-Space": "autocomplete",
 	fallthrough: ["default"]
