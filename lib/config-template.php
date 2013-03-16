12f8315a84004063f7e3c09509cba77808048d60
Matt Pass <matt@mattpass.com>
2013-03-16 12:06:01 +0000

    Voke.fm added as a plugin

--- lib/config-template.php
+++ lib/config-template.php
@@ -18,6 +18,7 @@ $ICEcoder = array(
 			array("Emmet","plugins/emmet/icon.png","margin-top: 3px","http://docs.emmet.io","_blank",""),
 			array("ICErepo","plugins/ice-repo/icon.png","margin-top: 3px","plugins/ice-repo","_blank",""),
 			array("Dochub","plugins/dochub/icon.png","margin-top: 3px","http://dochub.io","_blank",""),
+			array("Voke","plugins/voke/icon.png","margin-top: 3px","http://voke.fm","_blank",""),
 			array("JS Regex","plugins/jsregex/icon.png","margin-top: 3px","http://jsregex.herokuapp.com","_blank",""),
 			array("wireframe|cc","plugins/wireframecc/icon.png","margin-top: 3px","http://wireframe.cc","_blank",""),
 			array("Zip It!","plugins/zip-it/icon.png","margin-top: 3px; margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
