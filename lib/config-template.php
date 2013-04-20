7312789e12bc99322a7dd029266094c7c4f51c3c
Matt Pass <matt@mattpass.com>
2013-04-20 17:50:04 +0100

    jsRegex plugin out, Regexplained in
    jsRegex has been broken for a few weeks now and Regexplained has caught
    my attention as a very good tool to understand regexes you're writing.
    Swapped to this alternative.

--- lib/config-template.php
+++ lib/config-template.php
@@ -19,7 +19,7 @@ $ICEcoder = array(
 			array("ICErepo","plugins/ice-repo/icon.png","margin-top: 3px","plugins/ice-repo","_blank",""),
 			array("Dochub","plugins/dochub/icon.png","margin-top: 3px","http://dochub.io","_blank",""),
 			array("Voke","plugins/voke/icon.png","margin-top: 3px","http://voke.fm","_blank",""),
-			array("JS Regex","plugins/jsregex/icon.png","margin-top: 3px","http://jsregex.herokuapp.com","_blank",""),
+			array("Regexplained","plugins/regexplained/icon.png","margin-top: 3px","http://leaverou.github.io/regexplained","_blank",""),
 			array("wireframe|cc","plugins/wireframecc/icon.png","margin-top: 3px","http://wireframe.cc","_blank",""),
 			array("TinyPNG","plugins/tinypng/icon.png","margin-top: 7px","http://tinypng.org","_blank",""),
 			array("Zip It!","plugins/zip-it/icon.png","margin-top: 3px; margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
