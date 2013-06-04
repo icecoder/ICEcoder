ba520e6aa18bce66f19a32affabc8119067a6e35
Matt Pass <matt@mattpass.com>
2013-06-04 18:01:44 +0100

    Added config settings file
    These were buried in the settings.php file, now pulled out of the file
    and put into their own array
    This is so they can be found easier

---
+++ lib/config___settings.php
@@ -0,0 +1,11 @@
+<?php
+// ICEcoder system settings
+$ICEcoderSettings = array(
+	"versionNo"		=> "2.5",
+	"codeMirrorDir"		=> "CodeMirror-3.13",
+	"demoMode"		=> false,
+	"devMode"		=> false,
+	"multiUser"		=> false,
+	"lineEnding"		=> "\n"
+);
+?>
