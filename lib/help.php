f160b4fa80badf327b6c071db17483a1dc15db95
Matt Pass <matt@mattpass.com>
2013-06-04 17:39:03 +0100

    Range select added, shuffle of layout and Ctrl
    Range select (Shift + click) added as a shortcut
    File manager shortcuts moved under document shortcuts
    All instances of CTRL now Ctrl

--- lib/help.php
+++ lib/help.php
@@ -17,49 +17,50 @@
 
 <div style="display: inline-block; width: 365px; margin-right: 20px">
 	<h2>Within document</h2>
-	<span class="key">CTRL <span class="plus">+</span> &uarr;</span> <span class="shortcut">Move line up</span><br>
-	<span class="key">CTRL <span class="plus">+</span> &darr;</span> <span class="shortcut">Move line down</span><br>
-	<span class="key">CTRL <span class="plus">+</span> +</span> <span class="shortcut">Duplicate line</span><br>
-	<span class="key">CTRL <span class="plus">+</span> -</span> <span class="shortcut">Remove line</span><br>
-	<span class="key">CTRL <span class="plus">+</span> i</span> <span class="shortcut">Search selected text</span><br>
-	<span class="key">CTRL <span class="plus">+</span> j</span> <span class="shortcut">Jump to definition</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> &uarr;</span> <span class="shortcut">Move line up</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> &darr;</span> <span class="shortcut">Move line down</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> +</span> <span class="shortcut">Duplicate line</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> -</span> <span class="shortcut">Remove line</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> i</span> <span class="shortcut">Search selected text</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> j</span> <span class="shortcut">Jump to definition</span><br>
 	<span class="key">Esc</span> <span class="shortcut">Comment / uncomment text</span><br>
 	<span class="key">Tab</span> <span class="shortcut">Insert tab / indent selected text</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> d</span> <span class="shortcut">Wrap selected in &lt;div&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> s</span> <span class="shortcut">Wrap selected in &lt;span&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> p</span> <span class="shortcut">Wrap selected in &lt;p&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> a</span> <span class="shortcut">Wrap selected in &lt;a&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> b</span> <span class="shortcut">Wrap selected in &lt;b&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> i</span> <span class="shortcut">Wrap selected in &lt;i&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> g</span> <span class="shortcut">Wrap selected in &lt;strong&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> e</span> <span class="shortcut">Wrap selected in &lt;em&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> nums 1 - 7</span> <span class="shortcut">Wrap selected in &lt;h1&gt; - &lt;h7&gt;</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Alt <span class="plus">+</span> Enter</span> <span class="shortcut">End line with &lt;br&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> d</span> <span class="shortcut">Wrap selected in &lt;div&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> s</span> <span class="shortcut">Wrap selected in &lt;span&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> p</span> <span class="shortcut">Wrap selected in &lt;p&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> a</span> <span class="shortcut">Wrap selected in &lt;a&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> b</span> <span class="shortcut">Wrap selected in &lt;b&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> i</span> <span class="shortcut">Wrap selected in &lt;i&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> g</span> <span class="shortcut">Wrap selected in &lt;strong&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> e</span> <span class="shortcut">Wrap selected in &lt;em&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> nums 1 - 7</span> <span class="shortcut">Wrap selected in &lt;h1&gt; - &lt;h7&gt;</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Alt <span class="plus">+</span> Enter</span> <span class="shortcut">End line with &lt;br&gt;</span><br><br>
+
+	<h2>On logo</h2>
+	<span class="key">Left click</span> <span class="shortcut">Help</span><br>
+	<span class="key">Right click</span> <span class="shortcut">Settings</span><br>
 </div>
 
 <div style="display: inline-block; width: 365px">
-	<h2>On logo</h2>
-	<span class="key">Left click</span> <span class="shortcut">Help</span><br>
-	<span class="key">Right click</span> <span class="shortcut">Settings</span><br><br>
-
 	<h2>Within file manager</h2>
 	<span class="key">Left click</span> <span class="shortcut">Select file / folder</span><br>
 	<span class="key">Double click / click (mobile)</span> <span class="shortcut">Open file</span><br>
-	<span class="key">CTRL <span class="plus">+</span> left click</span> <span class="shortcut">Multiple select</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> left click</span> <span class="shortcut">Multiple select</span><br>
+	<span class="key">Shift <span class="plus">+</span> left click</span> <span class="shortcut">Range select</span><br>
 	<span class="key">Right click</span> <span class="shortcut">Options for selected</span><br>
 	<span class="key">Delete</span> <span class="shortcut">Delete selected</span><br><br>
 
 	<h2>Anywhere</h2>
-	<span class="key">CTRL <span class="plus">+</span> &rarr;</span> <span class="shortcut">Next tab</span><br>
-	<span class="key">CTRL <span class="plus">+</span> &larr;</span> <span class="shortcut">Previous tab</span><br>
-	<span class="key">CTRL <span class="plus">+</span> +</span> <span class="shortcut">New tab</span><br>
-	<span class="key">CTRL <span class="plus">+</span> -</span> <span class="shortcut">Close current tab</span><br>
-	<span class="key">CTRL <span class="plus">+</span> o</span> <span class="shortcut">Open file prompt</span><br>
-	<span class="key">CTRL <span class="plus">+</span> f</span> <span class="shortcut">Find</span><br>
-	<span class="key">CTRL <span class="plus">+</span> g</span> <span class="shortcut">Focus on Go to line input</span><br>
-	<span class="key">CTRL <span class="plus">+</span> s</span> <span class="shortcut">Save</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Shift <span class="plus">+</span> s</span> <span class="shortcut">Save as...</span><br>
-	<span class="key">CTRL <span class="plus">+</span> Enter</span> <span class="shortcut">View webpage</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> &rarr;</span> <span class="shortcut">Next tab</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> &larr;</span> <span class="shortcut">Previous tab</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> +</span> <span class="shortcut">New tab</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> -</span> <span class="shortcut">Close current tab</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> o</span> <span class="shortcut">Open file prompt</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> f</span> <span class="shortcut">Find</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> g</span> <span class="shortcut">Focus on Go to line input</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> s</span> <span class="shortcut">Save</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Shift <span class="plus">+</span> s</span> <span class="shortcut">Save as...</span><br>
+	<span class="key">Ctrl <span class="plus">+</span> Enter</span> <span class="shortcut">View webpage</span><br>
 	<span class="key">Space</span> <span class="shortcut">Refocus on document</span><br>
 	<span class="key">Esc</span> <span class="shortcut">Cancel tasks</span><br>
 
