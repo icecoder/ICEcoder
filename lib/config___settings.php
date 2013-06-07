f071f84eb463d7f83db43a4a9bbaace947a3b846
Matt Pass <matt@mattpass.com>
2013-06-07 12:46:37 +0100

    Version 3.0 beta

--- lib/config___settings.php
+++ lib/config___settings.php
@@ -1,7 +1,7 @@
 <?php
 // ICEcoder system settings
 $ICEcoderSettings = array(
-	"versionNo"		=> "2.5",
+	"versionNo"		=> "3.0 beta",
 	"codeMirrorDir"		=> "CodeMirror-3.13",
 	"docRoot"		=> $_SERVER['DOCUMENT_ROOT'],
 	"demoMode"		=> false,
