6765770deab763a138342e055b8b0f7ecc263bbd
Matt Pass <matt@mattpass.com>
2013-09-11 10:51:47 +0100

    Revised & reduced plugins
    Removed Dochub, Regexplained, TinyPNG, Voke and Wireframe CC plugins
    (they were only URLs to useful services, didn't integrate with ICEcoder)
    Remaining icons revised alpha channels where needed to work on dark BG

--- lib/config___users-template.php
+++ lib/config___users-template.php
@@ -14,15 +14,10 @@ $ICEcoderUserSettings = array(
 "bannedPaths"		=> array("/var/www/.git","/var/www/sites/all/modules","/var/www/sites/default/files"),
 "allowedIPs"		=> array("*"),
 "plugins"		=> array(
-			array("Adminer\\nMySQL database manager","plugins/adminer/icon.png","margin-top: 3px","plugins/adminer/adminer-3.7.1-mysql-en.php","_blank",""),
-			array("Emmet\\nSnippet type booster","plugins/emmet/icon.png","margin-top: 3px","http://docs.emmet.io","_blank",""),
-			array("ICErepo\\nGithub repo manager","plugins/ice-repo/icon.png","margin-top: 3px","plugins/ice-repo","_blank",""),
-			array("Dochub\\nCoding syntax info","plugins/dochub/icon.png","margin-top: 3px","http://dochub.io","_blank",""),
-			array("Voke\\nCoding syntax info","plugins/voke/icon.png","margin-top: 3px","http://voke.fm","_blank",""),
-			array("Regexplained\\nRegex builder","plugins/regexplained/icon.png","margin-top: 3px","http://leaverou.github.io/regexplained","_blank",""),
-			array("wireframe|cc\\nWireframing tool","plugins/wireframecc/icon.png","margin-top: 3px","http://wireframe.cc","_blank",""),
-			array("TinyPNG\\nImage file compressor","plugins/tinypng/icon.png","margin-top: 7px","http://tinypng.org","_blank",""),
-			array("Zip It!\\nFile/folder zip utility","plugins/zip-it/icon.png","margin-top: 3px; margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
+			array("<b>Adminer</b><br>nMySQL database manager","plugins/adminer/icon.png","margin-left: 2px","plugins/adminer/adminer-3.7.1-mysql-en.php","_blank",""),
+			array("<b>Emmet</b><br>Snippet type booster","plugins/emmet/icon.png","","http://docs.emmet.io","_blank",""),
+			array("<b>ICErepo</b><br>nGithub repo manager","plugins/ice-repo/icon.png","","plugins/ice-repo","_blank",""),
+			array("<b>Zip It!</b><br>nFile/folder zip utility","plugins/zip-it/icon.png","margin-left: 1px; margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
 			),
 "theme"			=> "default",
 "fontSize"		=> "13px",
