3388e090bbf7c174ade12c2f279f02c9632136da
Matt Pass <matt@mattpass.com>
2013-09-12 17:50:09 +0100

    Remove dup margin-left, should be 3px

--- lib/config___users-template.php
+++ lib/config___users-template.php
@@ -17,7 +17,7 @@ $ICEcoderUserSettings = array(
 			array("<b>Adminer</b><br>nMySQL database manager","plugins/adminer/icon.png","margin-left: 2px","plugins/adminer/adminer-3.7.1-mysql-en.php","_blank",""),
 			array("<b>Emmet</b><br>Snippet type booster","plugins/emmet/icon.png","","http://docs.emmet.io","_blank",""),
 			array("<b>ICErepo</b><br>nGithub repo manager","plugins/ice-repo/icon.png","","plugins/ice-repo","_blank",""),
-			array("<b>Zip It!</b><br>nFile/folder zip utility","plugins/zip-it/icon.png","margin-left: 1px; margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
+			array("<b>Zip It!</b><br>nFile/folder zip utility","plugins/zip-it/icon.png","margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
 			),
 "theme"			=> "default",
 "fontSize"		=> "13px",
