89e34fb24e95818bddcc6f6f7d673db019dc4c2c
Matt Pass <matt@mattpass.com>
2013-09-19 20:07:22 +0100

    No margin needed on new icon

--- lib/config___users-template.php
+++ lib/config___users-template.php
@@ -17,7 +17,7 @@ $ICEcoderUserSettings = array(
 "plugins"		=> array(
 			array("<b>Adminer</b><br>nMySQL database manager","plugins/adminer/icon.png","margin-left: 2px","plugins/adminer/adminer-3.7.1-mysql-en.php","_blank",""),
 			array("<b>Emmet</b><br>Snippet type booster","plugins/emmet/icon.png","","http://docs.emmet.io","_blank",""),
-			array("<b>CSS Beautify</b><br>Tidy up your CSS","plugins/cssbeautify/icon.png","margin-left: 4px","plugins/cssbeautify","fileControl:<b>Tidying your CSS</b>",""),
+			array("<b>CSS Beautify</b><br>Tidy up your CSS","plugins/cssbeautify/icon.png","","plugins/cssbeautify","fileControl:<b>Tidying your CSS</b>",""),
 			array("<b>ICErepo</b><br>nGithub repo manager","plugins/ice-repo/icon.png","","plugins/ice-repo","_blank",""),
 			array("<b>Zip It!</b><br>nFile/folder zip utility","plugins/zip-it/icon.png","margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
 			),
