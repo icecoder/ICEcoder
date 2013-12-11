8918abde7ee1bbbc2fdd7a148905e91869b4b213
Matt Pass <matt@mattpass.com>
2013-12-11 06:42:02 +0000

    Remove HTML tags
    These only display in title and alt tags, so should just be plain text

--- lib/config___users-template.php
+++ lib/config___users-template.php
@@ -15,11 +15,11 @@ $ICEcoderUserSettings = array(
 "bannedPaths"		=> array("/var/www/.git","/var/www/sites/all/modules","/var/www/sites/default/files"),
 "allowedIPs"		=> array("*"),
 "plugins"		=> array(
-			array("<b>Adminer</b><br>nMySQL database manager","plugins/adminer/icon.png","margin-left: 2px","plugins/adminer/adminer-3.7.1-mysql-en.php","_blank",""),
-			array("<b>Emmet</b><br>Snippet type booster","plugins/emmet/icon.png","","http://docs.emmet.io","_blank",""),
-			array("<b>CSS Beautify</b><br>Tidy up your CSS","plugins/cssbeautify/icon.png","","plugins/cssbeautify","fileControl:<b>Tidying your CSS</b>",""),
-			array("<b>ICErepo</b><br>nGithub repo manager","plugins/ice-repo/icon.png","","plugins/ice-repo","_blank",""),
-			array("<b>Zip It!</b><br>nFile/folder zip utility","plugins/zip-it/icon.png","margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
+			array("Adminer MySQL database manager","plugins/adminer/icon.png","margin-left: 2px","plugins/adminer/adminer-3.7.1-mysql-en.php","_blank",""),
+			array("Emmet Snippet type booster","plugins/emmet/icon.png","","http://docs.emmet.io","_blank",""),
+			array("CSS Beautify Tidy up your CSS","plugins/cssbeautify/icon.png","","plugins/cssbeautify","fileControl:<b>Tidying your CSS</b>",""),
+			array("ICErepo Github repo manager","plugins/ice-repo/icon.png","","plugins/ice-repo","_blank",""),
+			array("Zip It! File/folder zip utility","plugins/zip-it/icon.png","margin-left: 3px","plugins/zip-it/?zip=|&exclude=*.doc*.gif*.jpg*.jpeg*.pdf*.png*.swf*.xml*.zip","fileControl:<b>Zipping Files</b>","30")
 			),
 "theme"			=> "default",
 "fontSize"		=> "13px",
