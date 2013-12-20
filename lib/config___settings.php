33b47df99bece2982aced7c84fcb45700ac50949
Matt Pass <matt@mattpass.com>
2013-12-20 10:35:06 +0000

    Upgrade to CodeMirror 3.20

--- lib/config___settings.php
+++ lib/config___settings.php
@@ -2,7 +2,7 @@
 // ICEcoder system settings
 $ICEcoderSettings = array(
 	"versionNo"		=> "3.3",
-	"codeMirrorDir"		=> "CodeMirror-3.19",
+	"codeMirrorDir"		=> "CodeMirror-3.20",
 	"docRoot"		=> $_SERVER['DOCUMENT_ROOT'],
 	"demoMode"		=> false,
 	"devMode"		=> false,
