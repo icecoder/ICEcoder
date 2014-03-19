0beb6df2c8de4d25f7815d474723d3ac2f0278f4
Matt Pass <matt@mattpass.com>
2014-03-19 18:46:28 +0000

    Erlang, Julia and YAML languages included
    Java is part of the C Like language group
    Julia available as part of CodeMirror minification helper, seperate JS
    file used for now

--- editor.php
+++ editor.php
@@ -12,10 +12,11 @@
 <!--
 codemirror-compressed.js
 incls:	codemirror
-modes:	clike, coffeescript, css, htmlmixed, javascript, less, markdown, php, python, ruby, sql & xml
+modes:	clike, coffeescript, css, erlang, htmlmixed, javascript, julia, less, markdown, php, python, ruby, sql & xml, yaml
 addon:	brace-fold, closetag, css-hint, html-hint, javascript-hint, javascript-lint, lint, match-highlighter, searchcursor, show-hint, sql-hint, trailingspace, xml-fold, xml-hint
 //-->
 <script src="<?php echo $ICEcoder["codeMirrorDir"]; ?>/lib/codemirror-compressed.js"></script>
+<script src="lib/julia.js"></script>
 <script src="jshint/jshint-2.1.4.min.js"></script>
 <script src="lib/mmd.js"></script>
 <script src="lib/foldcode.js"></script>
