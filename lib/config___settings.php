f08552c5e338b0f00c9dd35a5ca78d01d618e67b
Matt Pass <matt@mattpass.com>
2014-04-10 10:00:22 +0100

    Upgrade to CodeMirror 3.23

--- lib/config___settings.php
+++ lib/config___settings.php
@@ -2,7 +2,7 @@
 // ICEcoder system settings
 $ICEcoderSettings = array(
 	"versionNo"		=> "4.0 beta",
-	"codeMirrorDir"		=> "CodeMirror-3.22",
+	"codeMirrorDir"		=> "CodeMirror-3.23",
 	"docRoot"		=> $_SERVER['DOCUMENT_ROOT'],
 	"demoMode"		=> false,
 	"devMode"		=> false,
