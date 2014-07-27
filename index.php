cc152db8ba5009e9ee707976bee79f99f02b4b62
Matt Pass <matt@mattpass.com>
2014-07-27 12:46:47 +0100

    Semicolon moved, githubAuthTokenSet if value set
    CSRF value ends with a ; as per all other settings (moved from other
    line)
    If we have a githubAuthToken value (ie, set in our config settings
    file), update top.ICEcoder.githubAuthTokenSet to true so we don't ask
    user for it to be set in our session

--- index.php
+++ index.php
@@ -80,8 +80,11 @@ window.onbeforeunload = function() {
 		"top.ICEcoder.bugFilePaths = ['".implode("','",$ICEcoder["bugFilePaths"])."'];".
 		"top.ICEcoder.bugFileCheckTimer = ".$ICEcoder["bugFileCheckTimer"].";".
 		"top.ICEcoder.bugFileMaxLines = ".$ICEcoder["bugFileMaxLines"].";".
-		"top.ICEcoder.csrf = '".$_SESSION["csrf"]."'";
-?>;ICEcoder.init()<?php echo $updateMsg.$onLoadExtras;?>;top.ICEcoder.content.style.visibility='visible';top.ICEcoder.filesFrame.contentWindow.frames['processControl'].location.href = 'processes/on-load.php';" onResize="ICEcoder.setLayout()" onKeyDown="return ICEcoder.interceptKeys('coder',event);" onKeyUp="parent.ICEcoder.resetKeys(event);" onBlur="parent.ICEcoder.resetKeys(event);">
+		if($ICEcoder["githubAuthToken"] != "") {
+			echo "top.ICEcoder.githubAuthTokenSet = true;";
+		}
+		"top.ICEcoder.csrf = '".$_SESSION["csrf"]."';";
+?>ICEcoder.init()<?php echo $updateMsg.$onLoadExtras;?>;top.ICEcoder.content.style.visibility='visible';top.ICEcoder.filesFrame.contentWindow.frames['processControl'].location.href = 'processes/on-load.php';" onResize="ICEcoder.setLayout()" onKeyDown="return ICEcoder.interceptKeys('coder',event);" onKeyUp="parent.ICEcoder.resetKeys(event);" onBlur="parent.ICEcoder.resetKeys(event);">
 
 <div id="blackMask" class="blackMask" onClick="if (!ICEcoder.overPopup) {ICEcoder.showHide('hide',this)}" onContextMenu="return false">
 	<div class="popupVCenter">
