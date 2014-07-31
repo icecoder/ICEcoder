230e8a0983c9498e966fbc6a814c62a27635de9a
Matt Pass <matt@mattpass.com>
2014-07-31 16:28:04 +0100

    Break in echo needs a ; and continue with echo

--- index.php
+++ index.php
@@ -79,11 +79,11 @@ window.onbeforeunload = function() {
 		"top.ICEcoder.autoComplete = '".$ICEcoder["autoComplete"]."';".
 		"top.ICEcoder.bugFilePaths = ['".implode("','",$ICEcoder["bugFilePaths"])."'];".
 		"top.ICEcoder.bugFileCheckTimer = ".$ICEcoder["bugFileCheckTimer"].";".
-		"top.ICEcoder.bugFileMaxLines = ".$ICEcoder["bugFileMaxLines"].";".
+		"top.ICEcoder.bugFileMaxLines = ".$ICEcoder["bugFileMaxLines"].";";
 		if($ICEcoder["githubAuthToken"] != "") {
 			echo "top.ICEcoder.githubAuthTokenSet = true;";
 		}
-		"top.ICEcoder.csrf = '".$_SESSION["csrf"]."';";
+		echo "top.ICEcoder.csrf = '".$_SESSION["csrf"]."';";
 ?>ICEcoder.init()<?php echo $updateMsg.$onLoadExtras;?>;top.ICEcoder.content.style.visibility='visible';top.ICEcoder.filesFrame.contentWindow.frames['processControl'].location.href = 'processes/on-load.php';" onResize="ICEcoder.setLayout()" onKeyDown="return ICEcoder.interceptKeys('coder',event);" onKeyUp="parent.ICEcoder.resetKeys(event);" onBlur="parent.ICEcoder.resetKeys(event);">
 
 <div id="blackMask" class="blackMask" onClick="if (!ICEcoder.overPopup) {ICEcoder.showHide('hide',this)}" onContextMenu="return false">
