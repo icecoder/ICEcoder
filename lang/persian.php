84d1402eefeeacdc02a8edbe4b84b1670d212dd0
Matt Pass <matt@mattpass.com>
2014-08-07 06:38:42 +0100

    Initial translations

---
+++ lang/persian.php
@@ -0,0 +1,121 @@
+<?php
+// Persian language translation
+// by: @voltan (GitHub)
+//     @azizabadi_ (Twitter)
+
+// Please preserve formatting, line breaks, special characters, anything in <tags> and HTML equivalents (eg &amp;). Translations on right side.
+
+$text = array(
+
+// / [ROOT LEVEL]
+
+	"editor" =>
+	array(
+		"Click icons for..."		=> "<b>بر روی آیکن ها کلیک کنید<br>برای راهنمایی &amp;<br>اطلاعات استفاده</b>",
+		"server"			=> "سرور",
+		"Server name, OS..."		=> "نام سرور ، سیستم عامل و آی پی :",
+		"Root"				=> "روت:",
+		"ICEcoder root"			=> "ICEcoder روت :",
+		"PHP version"			=> "نسخه پی اچ پی:",
+		"Date & time"			=> "زمان و تاریخ :",
+		"your device"			=> "دستگاه شما",
+		"Browser"			=> "مرورگر :",
+		"Your IP"			=> "آی پی شما:",
+		"files"				=> "فایل ها",
+		"Last 10 files..."		=> "آخرین ۱۰ فایل باز شده",
+		"none"				=> "[پوچ]",
+		"test suite"			=> "تست سویت",
+		"Run unit tests"		=> "اجرای تست های واحد",
+		"dev mode"			=> "حالت توسعه دهنده",
+		"Status"			=> "وضعیت",
+		"Using"				=> "استفاده",
+		"You can switch..."		=> "شما میتوانید حالت توسعه را از lib/config__settings.php روشن یا خاموش کنید",
+		"results"			=> "نتایج"
+
+	),
+
+	"files" =>
+	array(
+		"Lock"				=> "قفل",
+		"Refresh"			=> "بارگذاری مجدد",
+		"ROOT"				=> "[ریشه]"
+
+	),
+
+	"index" =>
+	array(
+		"UPDATE INFO"			=> "اطلاعات به روز رسانی",
+		"now available"			=> "هم اکنون در دسترس است",
+		"Your version is"		=> "نسخه مورد استفاده شما",
+		"Update now"			=> "به روز کنید",
+		"You have some..."		=> "شما چند تغییر ذخیره نشده دارید",
+		"working"			=> "در حال کار",
+		"Color picker"			=> "اشاره گر رنگ",
+		"New File"			=> "فایل جدید",
+		"New Folder"			=> "پوشه جدید",
+		"Upload File(s)"		=> "بارگذاری فایل",
+		"Paste"				=> "چسباندن",
+		"Open"				=> "باز کردن",
+		"Copy"				=> "کپی کردن",
+		"Duplicate"			=> "تهیه نسخه دوم",
+		"Delete"			=> "حذف",
+		"Rename"			=> "تغیر نام",
+		"View Webpage"			=> "نمایش وب سایت",
+		"Download"			=> "دریافت",
+		"Properties"			=> "ویژگی ها",
+		"File"				=> "فایل",
+		"Edit"				=> "ویرایش",
+		"Remote"			=> "کنترل از راه دور",
+		"Help"				=> "کمک",
+		"Save"				=> "ذخیره",
+		"Save As"			=> "ذخیره به عنوان",
+		"Live Preview"			=> "پیش نمایش زنده",
+		"Upload"			=> "بارگذاری",
+		"Zip"				=> "زیپ",
+		"Print"				=> "چاپ",
+		"Fullscreen toggle"		=> "نمایش تمام صفحه",
+		"Logout"			=> "خروج",
+		"Undo"				=> "برگشت",
+		"Redo"				=> "ازنو",
+		"Indent more"			=> "کم کردن فاصله",
+		"Indent less"			=> "زیاد کردن فاصله",
+		"Autocomplete"			=> "تکمیل خودکار",
+		"Comment/Uncomment"		=> "نظر/ضد نظر",
+		"Jump to Definition"		=> "انتقال به تعریف",
+		"Manual"			=> "دستی",
+		"Shortcuts"			=> "کلید های میانبر",
+		"Settings"			=> "تنظیمات",
+		"Search for selected"		=> "جستجو برای انتخاب شده ها",
+		"website"			=> "وب سایت",
+		"Close all tabs"		=> "بستن همه پنجره ها",
+		"Alphabetize tabs"		=> "مرتب سازی پنجره ها بر اساس حروف الفبا",
+		"Find"				=> "یافتن",
+		"in"				=> "در",
+		"and"				=> "و",
+		"replace"			=> "جایگزین کردن",
+		"replace all"			=> "جایگزین کردن همه",
+		"this document"			=> "این سند",
+		"open documents"		=> "سند های باز",
+		"all files"			=> "همه فایل ها",
+		"all filenames"			=> "همه نام فایل ها",
+		"Turn on/off..."		=> "روشن / خاموش کردن کد کمد",
+		"Code Assist"			=> "کد کمک",
+		"Go to Line"			=> "برو به خط",
+		"View"				=> "نمایش",
+		"Bug reporting not active"	=> "گزارش اشکال فعال نیست"
+	),
+
+// /LIB
+
+	"login" =>
+	array(
+		"set password"			=> "تنظیم واژه رمز",
+		"login"				=> "ورود",
+		"To disable registration..."	=> "برای غیر فعال کردن حالت ثبت نام ، منوی تنظیمات را باز کنید و یا فایل lib/config___settings.php را باز کنید و گزینه enableRegistration را به false تغییر دهید. سپس این صفحه را بارگذاری مجدد نمایید",
+		"Registration mode enabled"	=> "حالت ثبت نام فعال است",
+		"auto-check for updates"	=> "بررسی خودکار برای به روز رسانی",
+		"To put into..."		=> "برای وارد شدن به حالت چند کاربره ، منوی تنظیمات را باز کنید و یا فایل lib/config___settings.php و گزینه multiUser را به true تغییر دهید و و ذخیره کنید. سپس این صفحه را بارگذاری مجدد نمایید",
+		"multi-user"			=> "چند کاربره"
+	)
+);
+?>
