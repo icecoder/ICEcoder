ba6e0c0db917ec28502417ba547d2666e56ff28c
lzh <lzh370@qq.com>
2014-08-07 10:24:36 +0800

    chinese.php
    Add chinese.php

---
+++ lang/chinese.php
@@ -0,0 +1,121 @@
+<?php
+// Chinese language translation
+// by: @lzh370 (GitHub)
+//     @lzh370 (Twitter)
+
+// Please preserve formatting, line breaks, special characters, anything in <tags> and HTML equivalents (eg &amp;). Translations on right side.
+
+$text = array(
+
+// / [ROOT LEVEL]
+
+	"editor" =>
+	array(
+		"Click icons for..."		=> "<b>点击图标<br>求助 &amp;<br>使用信息</b>",
+		"server"			=> "服务器",
+		"Server name, OS..."		=> "服务器名称, OS & IP:",
+		"Root"				=> "根目录:",
+		"ICEcoder root"			=> "ICEcoder 根目录:",
+		"PHP version"			=> "PHP 版本:",
+		"Date & time"			=> "日期 & 时间:",
+		"your device"			=> "你的设备",
+		"Browser"			=> "浏览器:",
+		"Your IP"			=> "你的 IP:",
+		"files"				=> "文件",
+		"Last 10 files..."		=> "最后10个打开的文件:",
+		"none"				=> "[none]",
+		"test suite"			=> "测试套件",
+		"Run unit tests"		=> "运行单元测试",
+		"dev mode"			=> "开发模式",
+		"Status"			=> "Status",
+		"Using"				=> "Using",
+		"You can switch..."		=> "你可以在 lib/config__settings.php 中设置开发模式开关",
+		"results"			=> "结果"
+
+	),
+
+	"files" =>
+	array(
+		"Lock"				=> "锁定",
+		"Refresh"			=> "刷新",
+		"ROOT"				=> "[ROOT]"
+
+	),
+
+	"index" =>
+	array(
+		"UPDATE INFO"			=> "更新信息",
+		"now available"			=> "现在可以",
+		"Your version is"		=> "你的版本是",
+		"Update now"			=> "现在更新",
+		"You have some..."		=> "你有一些未保存的更改",
+		"working"			=> "载入中",
+		"Color picker"			=> "颜色选择器",
+		"New File"			=> "新建文件",
+		"New Folder"			=> "新建文件夹",
+		"Upload File(s)"		=> "上传文件",
+		"Paste"				=> "粘贴",
+		"Open"				=> "打开",
+		"Copy"				=> "复制",
+		"Duplicate"			=> "副本",
+		"Delete"			=> "删除",
+		"Rename"			=> "重命名",
+		"View Webpage"			=> "预览网页",
+		"Download"			=> "下载",
+		"Properties"			=> "属性",
+		"File"				=> "文件",
+		"Edit"				=> "编辑",
+		"Remote"			=> "远程",
+		"Help"				=> "帮助",
+		"Save"				=> "保存",
+		"Save As"			=> "另存为",
+		"Live Preview"			=> "实时预览",
+		"Upload"			=> "上传",
+		"Zip"				=> "压缩",
+		"Print"				=> "打印",
+		"Fullscreen toggle"		=> "全屏切换",
+		"Logout"			=> "退出",
+		"Undo"				=> "撤销",
+		"Redo"				=> "重做",
+		"Indent more"			=> "增加缩进",
+		"Indent less"			=> "减少缩进",
+		"Autocomplete"			=> "自动完成",
+		"Comment/Uncomment"		=> "注释 / 清除注释",
+		"Jump to Definition"		=> "跳转至指定行",
+		"Manual"			=> "手册",
+		"Shortcuts"			=> "快捷键",
+		"Settings"			=> "设置",
+		"Search for selected"		=> "搜索选择",
+		"website"			=> "网站",
+		"Close all tabs"		=> "关闭所有选项卡",
+		"Alphabetize tabs"		=> "按字母顺序排列的选项卡",
+		"Find"				=> "查找",
+		"in"				=> "查找",
+		"and"				=> "替换",
+		"replace"			=> "替换",
+		"replace all"			=> "替换所有",
+		"this document"			=> "当前文件",
+		"open documents"		=> "打开的文件",
+		"all files"			=> "所有文件",
+		"all filenames"			=> "所有文件名",
+		"Turn on/off..."		=> "开启/关闭代码辅助",
+		"Code Assist"			=> "代码辅助",
+		"Go to Line"			=> "转到行",
+		"View"				=> "预览",
+		"Bug reporting not active"	=> "错误报告没有激活"
+	),
+
+// /LIB
+
+	"login" =>
+	array(
+		"set password"			=> "设置密码",
+		"login"				=> "登陆",
+		"To disable registration..."	=> "要禁用注册模式, 请打开设置或打开文件 lib/config___settings.php 并更改 enableRegistration 为 false , 并重新载入本页面",
+		"Registration mode enabled"	=> "注册模式已启用",
+		"auto-check for updates"	=> "自动检测更新",
+		"To put into..."		=> "要禁用多用户模式, 请打开设置或打开文件 lib/config___settings.php 并更改 multiUser 为 true , 并重新载入本页面",
+		"multi-user"			=> "多用户"
+	)
+);
+?>
