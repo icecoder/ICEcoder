2d76f1f7f362ffed89f1bf165568555b01105f21
kris <kris@xoofoo.org>
2014-08-10 02:23:30 +0200

    Update french language
    More definitions translated

--- lang/french.php
+++ lang/french.php
@@ -12,7 +12,7 @@ $text = array(
 	"editor" =>
 	array(
 		"Click icons for..."		=> "<b>Cliquez sur les ic&ocirc;nes<br>pour l&apos;aide &amp;<br>et infos d&apos;utilisation</b>",
-		"server"				=> "serveur",
+		"server"				=> "Serveur",
 		"Server name, OS..."	=> "Nom du serveur, OS & IP :",
 		"Root"				=> "Racine :",
 		"ICEcoder root"		=> "Racine d&apos;ICEcoder :",
@@ -21,7 +21,7 @@ $text = array(
 		"your device"			=> "Votre &eacute;quipement",
 		"Browser"				=> "Navigateur :",
 		"Your IP"				=> "Votre IP :",
-		"files"				=> "fichiers",
+		"files"				=> "Fichiers",
 		"Last 10 files..."		=> "10 derniers fichiers ouverts :",
 		"none"				=> "[aucun]",
 		"test suite"			=> "Suite de test",
@@ -90,33 +90,66 @@ dans lib/config__settings.php",
 		"website"				=> "Site internet",
 		"Close all tabs"		=> "Fermer les onglets",
 		"Alphabetize tabs"		=> "Onglets par ordre alphab&eacute;tique",
-		"Find"				=> "Trouver",
+		"Find"				=> "Chercher",
 		"in"					=> "dans",
 		"and"				=> "et",
 		"replace"				=> "remplacer",
 		"replace all"			=> "remplacer tout",
 		"this document"		=> "ce document",
-		"open documents"		=> "ouvrir les documents",
-		"all files"				=> "tous les ficheirs",
+		"open documents"		=> "les documents ouverts",
+		"all files"				=> "tous les fichiers",
 		"all filenames"			=> "tous les noms de fichiers",
 		"Turn on/off..."			=> "Basculer on/off l&apos;assistance au code",
 		"Code Assist"			=> "Assistance au Code",
 		"Go to Line"			=> "Aller &agrave; la ligne",
 		"View"				=> "Afficher",
-		"Bug reporting not active"	=> "La communication de dysfonctionnement n&apos;est pas actif"
+		"Bug reporting not active"	=> "Le signalement de dysfonctionnement n&apos;est pas actif"
 	),
 
 // /LIB
 
+	"bug-files-check" =>
+	array(
+		"Found in"			=> "Trouv&eacute; dans :"
+	),
+
+	"file-control" =>
+	array(
+		"Sorry"				=> "D&eacute;sol&eacute;",
+		"does not seem..."		=> "ne semble pas exister sur le serveur",
+		"Sorry, could not..."		=> "D&eacute;sol&eacute;, impossibilit&eacute; de r&eacute;cup&eacute;rer le contenu de",
+		"Sorry, cannot create..."	=> "D&eacute;sol&eacute;, impossibilit&eacute; de cr&eacute;er le dossier sur",
+		"Sorry, cannot copy"		=> "D&eacute;sol&eacute;, impossibilit&eacute; de copier",
+		"into"				=> "dans",
+		"Uploaded file(s) OK"	=> "T&eacute;l&eacute;chargement du (des) fichier(s) r&eacute;ussi(s)",
+		"Sorry, cannot upload"	=> "D&eacute;sol&eacute;, impossibilit&eacute; de t&eacute;l&eacute;charger",
+		"Sorry, cannot upload..."	=> "D&eacute;sol&eacute;, impossibilit&eacute; de t&eacute;l&eacute;charger en mode d&eacute;mo",
+		"Sorry, cannot rename"	=> "D&eacute;sol&eacute;, impossibilit&eacute; de renommer",
+		"Maybe public write..."	=> " Peut-&ecirc;tre que les permissions d&apos;&eacute;criture publique sont n&eacute;cessaires sur ce dossier ou son r&eacute;pertoire ?",
+		"Sorry, cannot move"	=> "D&eacute;sol&eacute;, impossibilit&eacute; de d&eacute;placer",
+		"Sorry, cannot save"		=> "D&eacute;sol&eacute;, impossibilit&eacute; de sauvegarder",
+		"Sorry, cannot replace..."	=> "D&eacute;sol&eacute;, impossibilit&eacute; de remplacer le texte dans ",
+		"Sorry, cannot change..."	=> "D&eacute;sol&eacute;, impossibilit&eacute; de changer les permissions sur",
+		"Sorry, cannot delete..."	=> "D&eacute;sol&eacute;, imossibilit&eacute; de supprimer le dossier racine",
+		"Sorry, cannot delete"	=> "D&eacute;sol&eacute;, impossibilit&eacute; d&apos;effacer",
+		"Sorry, this file..."		=> "D&eacute;sol&eacute;, le fichier a &eacute;t&eacute; modifi&eacute;, impossibilit&eacute; de sauvegarder",
+		"Reload this file..."		=> "Recharger le fichier, et copier votre version dans un nouveau document ?",
+		"There was a..."		=> "Un probl&egrave;me technique a &eacute;t&eacute; rencontr&eacute;, probablement quelque chose qui n&apos;&eacute;tait pas tout &agrave; fait pr&ecirc;t. ICEcoder a donc relanc&eacute; &agrave; nouveau son contr&ocirc;le de fichier.",
+		"displayed at"			=> "affich&eacute; sur",
+		"Enter filename to..."	=> "Saisissez le nom du fichier &agrave; enregistrer sur",
+		"That file exists..."		=> "Ce fichier existe d&eacute;j&agrave;, voulez vous l&apos;&eacute;craser ?",
+		"Saving"				=> "Sauvegard&eacute;"
+	),	
+
 	"login" =>
 	array(
-		"set password"			=> "d&eacute;finir un mot de passe",
+		"set password"			=> "D&eacute;finir un mot de passe",
 		"login"				=> "Connexion",
 		"To disable registration..."=> "Pour d&eacute;sactiver le mode &apos;enregsitrement&apos;, ouvrir le menu des pr&eacute;f&eacute;rences ou le fichier lib/config___settings.php et modifier enableRegistration &agrave; &apos;false&apos;, puis recharger cette page",
 		"Registration mode enabled"	=> "Mode Enregistrement activ&eacute;",
-		"auto-check for updates"	=> "v&eacute;rification automatique pour les mises &agrave; jour",
+		"auto-check for updates"	=> "V&eacute;rification automatique des mises &agrave; jour",
 		"To put into..."			=> "Pour passer en mode &apos;multi-utilisateur&apos;, ouvrir le menu des pref&eacute;rences ou le ifchier lib/config___settings.php et modifier multiUser &agrave; &apos;true&apos;, puis recharger cette page",
-		"multi-user"			=> "multi-utilisateur"
+		"multi-user"			=> "Multi-utilisateur"
 	)
 );
 ?>
