bc9639dac29d76030bbf1f7258ab4b037250e327
lzh <lzh370@qq.com>
2014-08-19 11:55:01 +0800

    Supplementary Chinese translation

--- lang/chinese-simplified.php
+++ lang/chinese-simplified.php
@@ -1,7 +1,7 @@
 ﻿<?php
 // Chinese language translation
 // by: @lzh370 (GitHub)
-//     @lzh370 (Twitter)
+//     lzh370@qq.com
 
 // Please preserve formatting, line breaks, special characters, anything in <tags> and HTML equivalents (eg &amp;). Translations on right side.
 
@@ -107,6 +107,139 @@ $text = array(
 
 // /LIB
 
+	"bug-files-check" =>
+	array(
+		"Found in"			=> "发现在:"
+	),
+
+	"file-control" =>
+	array(
+		"Sorry"				=> "抱歉",
+		"does not seem..."		=> "似乎不存在于服务器上",
+		"Sorry, could not..."		=> "抱歉, 不能得到的内容",
+		"Sorry, cannot create..."	=> "抱歉, 不能创建文件夹",
+		"Sorry, cannot copy"		=> "抱歉, 不能复制",
+		"into"				=> "到",
+		"Uploaded file(s) OK"		=> "上传文件完成",
+		"Sorry, cannot upload"		=> "抱歉, 不能上传",
+		"Sorry, cannot upload..."	=> "抱歉, 在演示模式不能上传文件",
+		"Sorry, cannot rename"		=> "抱歉, 不能重命名",
+		"Maybe public write..."		=> "也许父文件夹也需要写入权限?",
+		"Sorry, cannot move"		=> "抱歉, 不能移动",
+		"Sorry, cannot save"		=> "抱歉, 不能保存",
+		"Sorry, cannot replace..."	=> "抱歉, 目标不能替换文本",
+		"Sorry, cannot change..."	=> "抱歉, 目标不能更改权限",
+		"Sorry, cannot delete..."	=> "抱歉, 不能删除根级别ROOT",
+		"Sorry, cannot delete"		=> "抱歉, 不能删除",
+		"Sorry, this file..."		=> "抱歉, 此文件已更改, 不能保存",
+		"Reload this file..."		=> "重新加载该文件, 你的版本复制到一个新文件?",
+		"There was a..."		=> "这些代码还没有完全准备好. 所以 ICEcoder 需要再次重新加载.",
+		"displayed at"			=> "显示在",
+		"Enter filename to..."		=> "输入文件名并保存到",
+		"That file exists..."		=> "该文件已存在, 要覆盖吗?",
+		"Saving"			=> "正在保"
+	),
+
+	"get-branch" =>
+	array(
+		"There are no..."		=> "本地文件与 GitHub repo 没有明显差异. 切换回普通模式?",
+		"Sorry, there was..."		=> "抱歉, 这里有一个错误, 代码:",
+		"Your local folder..."		=> "你的本地文件夹是空的, 你是否想克隆"
+	),
+
+	"github-manager" =>
+	array(
+		"Sorry, cannot create..."	=> "抱歉, 不能创建目录到",
+		"Cannot update config..."	=> "不能更新配置文件. 请对",
+		"and try again"			=> "增加写入权限, 并再次尝试",
+		"saving github paths"		=> "正在保存 github 路径...",
+		"github paths"			=> "github 路径",
+		"Choose existing path"		=> "选择现有路径",
+		"Local path"			=> "本地路径",
+		"Remote GitHub path"		=> "远程 GitHub 路径",
+		"Choose"			=> "选择",
+		"Set local and..."		=> "设置本地路径和远程路径需要去除空格",
+		"Update"			=> "更新",
+		"Add new path"			=> "增加新路径",
+		"Add"				=> "增加",
+		"Usage Info"			=> "使用方法:",
+		"Enter relative local..."	=> ">输入本地相对路径 (/server/myfiles) 和 GitHub 绝对路径 (https://github.com/user/repo 或 https://github.com/user/repo/tree/branch 各分支), 按照示例. 完成后你就建立了这两个地点的源路径, 它是成对数存在.",
+		"You can then..."		=> "你可以选择一个路径作为 ICEcoder 的根目录.",
+		"The file manager..."		=> "文件管理器右侧会显示一个新的GitHub的图标, 您可以点击执行, 会自动校验并显示本地与github源之间的差异. 这些差别可以被提交并推送到在GitHub上的远程路径或克隆到你的本地路径, 用来同步您的文件.",
+		"If you want..."		=> "如果你想设置的另一个根路径, 这可以在 帮助 > 设置窗口 中进行设置."
+	),
+
+	"github" =>
+	array(
+		"Sorry, you do..."		=> "抱歉, 您的服务器没有启用 OpenSSL 的 PHP 实例, 所以 https 目前不可用. GitHub 的数据传输必须要 https 连接, 请修改 php.ini 设置, 重启你的服务器并重新尝试"
+	),
+
+	"headers" =>
+	array(
+		"Bad CSRF token..."		=> "错误的 CSRF token. 请在 https://github.com/mattpass/ICEcoder 报告错误信息, 以便我们修复它."
+	),
+
+	"help" =>
+	array(
+		"shortcuts"			=> "快捷键",
+		"Within document"		=> "文档",
+		"On Tabs"			=> "选项卡",
+		"Within file manager"		=> "文件管理器",
+		"Anywhere"			=> "其他",
+		"Space"				=> "空格",
+		"Click"				=> "单击",
+		"or"				=> "或",
+		"Left click"			=> "左键单击",
+		"Middle click"			=> "中键单击",
+		"Double click tap..."		=> "双击 / 点击 (移动端)",
+		"Right click"			=> "右键单击",
+		"Middle scrollwheel"		=> "中间滚轮滚动",
+		"Drag"				=> "拖动",
+		"Autocomplete add snippet"	=> "自动完成 / 添加片段",
+		"Multiple select"		=> "多选",
+		"Move line up"			=> "向上移动行",
+		"Move line down"		=> "向下移动行",
+		"Duplicate lines"		=> "复制行",
+		"Remove lines"			=> "删除行",
+		"Insert line before"		=> "前插入行",
+		"Insert line after"		=> "后插入行",
+		"Search for selected"		=> "搜索选择",
+		"Jump to definition"		=> "跳转到 / 跳转回",
+		"Comment uncomment"		=> "注释 / 清除注释",
+		"Insert tab indent"		=> "插入tab / 插入选择",
+		"Wrap with div"			=> "封装 &lt;div&gt;",
+		"Wrap with span"		=> "封装 &lt;span&gt;",
+		"Wrap unwrap p"			=> "封装 / 撤销封装 with &lt;p&gt;",
+		"Wrap unwrap a"			=> "封装 / 撤销封装 with &lt;a&gt;",
+		"Wrap unwrap b"			=> "封装 / 撤销封装 with &lt;b&gt;",
+		"Wrap unwrap i"			=> "封装 / 撤销封装 with &lt;i&gt;",
+		"Wrap unwrap strong"		=> "封装 / 撤销封装 with &lt;strong&gt;",
+		"Wrap unwrap em"		=> "封装 / 撤销封装 with &lt;em&gt;",
+		"Wrap unwrap h1..."		=> "封装 / 撤销封装 with &lt;h1&gt; - &lt;h3&gt;",
+		"End line with..."		=> "换行 &lt;br&gt;",
+		"Close tab"			=> "关闭选项卡",
+		"Select file folder"		=> "选择文件 / 文件夹",
+		"Open file"			=> "打开文件",
+		"Range select"			=> "范围选择",
+		"Options for selected"		=> "所选的选项",
+		"Delete selected"		=> "删除已选",
+		"Next previous tab"		=> "下一个 / 上一个 选项卡",
+		"Next tab"			=> "下一个选项卡",
+		"Previous tab"			=> "上一个选项卡",
+		"New tab"			=> "新建选项卡",
+		"Close current tab"		=> "关闭当前选项卡",
+		"Open file prompt"		=> "打开文件的提",
+		"Find"				=> "搜索",
+		"Focus on Go..."		=> "光标定位到转到行的输入框",
+		"Save"				=> "保存",
+		"Save as"			=> "另存为...",
+		"View webpage"			=> "预览网页",
+		"Contract expand file..."	=> "收缩 / 扩展文件管理器",
+		"Fold unfold current..."	=> "折叠 / 展开当前行",
+		"Refocus on document"		=> "重新聚焦于文档",
+		"Cancel tasks"			=> "取消任务"
+	),
+
 	"login" =>
 	array(
 		"set password"			=> "设置密码",
@@ -116,6 +249,24 @@ $text = array(
 		"auto-check for updates"	=> "自动检测更新",
 		"To put into..."		=> "要禁用多用户模式, 请打开设置或打开文件 lib/config___settings.php 并更改 multiUser 为 true , 并重新载入本页面",
 		"multi-user"			=> "多用户"
+	),
+
+	"multiple-results" =>
+	array(
+		"rename all"			=> "重命名所有",
+		"replace all"			=> "全部替换",
+		"document"			=> "文档",
+		"Found"				=> "找到",
+		"times"				=> "时间",
+		"replace"			=> "替换",
+		"file folder"			=> "文件/文件夹",
+		"rename to"			=> "重命名为",
+		"rename"			=> "重命名",
+		"file"				=> "文件",
+		"No matches found"		=> "没有找到匹配",
+		"selected"			=> "选择",
+		"found in"			=> "查询结果在",
+		"Replaced"			=> "已替换"
 	)
 );
 ?>
