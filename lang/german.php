4e3d73e8b9a75f569a35f6756296b791a385546e
Oliver <oliver.strauss@gmail.com>
2014-08-21 14:38:10 +0200

    Create germna.php file
    Added german language file.

---
+++ lang/german.php
@@ -0,0 +1,273 @@
+<?php
+// English language translation
+// by: @mattpass (GitHub)
+//     @mattpass (Twitter)
+
+// Please preserve formatting, line breaks, special characters, anything in <tags> and HTML equivalents (eg &amp;). Translations on right side.
+
+$text = array(
+
+// / [ROOT LEVEL]
+
+	"editor" =>
+	array(
+		"Click icons for..."		=> "<b>Klick auf das Icon<br>f&uuml;r Hilfe &amp;<br>Anwendungsbeispielen</b>",
+		"server"			=> "Server",
+		"Server name, OS..."		=> "Servername, OS & IP:",
+		"Root"				=> "Root:",
+		"ICEcoder root"			=> "ICEcoder root:",
+		"PHP version"			=> "PHP Version:",
+		"Date & time"			=> "Datum & Uhrzeit:",
+		"your device"			=> "Dein Gerät",
+		"Browser"			=> "Browser:",
+		"Your IP"			=> "Deine IP:",
+		"files"				=> "Dateien",
+		"Last 10 files..."		=> "Die letzten 10 ge&ouml;ffneten Dateien:",
+		"none"				=> "[nichts]",
+		"test suite"			=> "Test Suite",
+		"Run unit tests"		=> "Unittest starten",
+		"dev mode"			=> "Entwicklermodus",
+		"Status"			=> "Status",
+		"Using"				=> "Verwenden",
+		"You can switch..."		=> "Du kannst den Entwicklermodus in der in lib/config__settings.php ein- und ausschateln",
+		"results"			=> "Ergebnis"
+
+	),
+
+	"files" =>
+	array(
+		"Lock"				=> "Sperren",
+		"Refresh"			=> "Erneueren",
+		"ROOT"				=> "[ROOT]"
+
+	),
+
+	"index" =>
+	array(
+		"UPDATE INFO"			=> "Update Infos",
+		"now available"			=> "jetzt verf&uuml;gbar",
+		"Your version is"		=> "Deine Version ist",
+		"Update now"			=> "Aktualisiere jetzt",
+		"You have some..."		=> "Du hast einige nicht gespeicherete Datein",
+		"working"			=> "arbeite",
+		"Color picker"			=> "Farbauswahl",
+		"New File"			=> "Neue Datei",
+		"New Folder"			=> "Neuer Ordner",
+		"Upload File(s)"		=> "Dateie(n) hochladen",
+		"Paste"				=> "Einf&uuml;gen",
+		"Open"				=> "&Ouml;ffnen",
+		"Copy"				=> "Kopiere",
+		"Duplicate"			=> "Duplizieren",
+		"Delete"			=> "L&ouml;schen",
+		"Rename"			=> "Umbenennen",
+		"View Webpage"			=> "Webseite anzeigen",
+		"Download"			=> "Herunterladen",
+		"Properties"			=> "Einstellungen",
+		"File"				=> "Datei",
+		"Edit"				=> "Editieren",
+		"Remote"			=> "Fernzugriff",
+		"Help"				=> "Hilfe",
+		"Save"				=> "Speichern",
+		"Save As"			=> "Speichern als",
+		"Live Preview"			=> "Live Vorschau",
+		"Upload"			=> "Hochladen",
+		"Zip"				=> "Zip",
+		"Print"				=> "Drucken",
+		"Fullscreen toggle"		=> "Vollbildmodus wechseln",
+		"Logout"			=> "Ausloggen",
+		"Undo"				=> "R&uuml;ckg&auml;ngig",
+		"Redo"				=> "Wiederherstellen",
+		"Indent more"			=> "Einzug vergr&ouml;&szlig;ern",
+		"Indent less"			=> "Einzug verkleinern",
+		"Autocomplete"			=> "Auto vervollst&auml;ndigen",
+		"Comment/Uncomment"		=> "Kommentieren/Auskommentieren",
+		"Jump to Definition"		=> "Springe zur Definition",
+		"Manual"			=> "Anleitung",
+		"Shortcuts"			=> "Schnellzugriffe",
+		"Settings"			=> "Einstellungen",
+		"Search for selected"		=> "Nach selektiertem suchen",
+		"website"			=> "Website",
+		"Close all tabs"		=> "Alle Tabs schlie&szlig;en",
+		"Alphabetize tabs"		=> "Tabs alphabetisch sortieren",
+		"Find"				=> "Finden",
+		"in"				=> "in",
+		"and"				=> "und",
+		"replace"			=> "ersetzen",
+		"replace all"			=> "alle ersetzen",
+		"this document"			=> "dieses Dokument",
+		"open documents"		=> "ge&ouml;ffnete Dokumente",
+		"all files"			=> "Alle Dateien",
+		"all filenames"			=> "Alle Dateinamen",
+		"Turn on/off..."		=> "Codeassistenten ein- ausschalten",
+		"Code Assist"			=> "Codeassistent",
+		"Go to Line"			=> "Geh zur Zeile",
+		"View"				=> "Anzeigen",
+		"Bug reporting not active"	=> "Fehlermeldungen sind nicht aktiv"
+	),
+
+// /LIB
+
+	"bug-files-check" =>
+	array(
+		"Found in"			=> "Gefunden in:"
+	),
+
+	"file-control" =>
+	array(
+		"Sorry"				=> "Entschuldigung",
+		"does not seem..."		=> "Es scheint auf dem Server nicht zu existieren",
+		"Sorry, could not..."		=> "Entschuldigung, kann den Inhalte von",
+		"Sorry, cannot create..."	=> "Entschuldigung, kann den Ordner nicht erstellen in",
+		"Sorry, cannot copy"		=> "Entschuldigung, kann nicht kopieren",
+		"into"				=> "in",
+		"Uploaded file(s) OK"		=> "Hoch geladene Datei(n) OK",
+		"Sorry, cannot upload"		=> "Entschuldigung, kann nicht hochladen",
+		"Sorry, cannot upload..."	=> "Entschuldigung, kann solange nicht hochladen, solange der Demo Modus aktiv ist",
+		"Sorry, cannot rename"		=> "Entschuldigung, kann nicht umbenennen",
+		"Maybe public write..."		=> "Vielleicht fehlen Schreibrechte auf dem &uuml;bergeordneten Ordner?",
+		"Sorry, cannot move"		=> "Entschuldigung, kann nicht verschieben",
+		"Sorry, cannot save"		=> "Entschuldigung, kann nicht speichern",
+		"Sorry, cannot replace..."	=> "Entschuldigung, kann den Text nicht ersetzen",
+		"Sorry, cannot change..."	=> "Entschuldigung, kann die Berechtigung nicht &auml;ndern f&uuml;r",
+		"Sorry, cannot delete..."	=> "Entschuldigung, kann das Hauptverzeichnis nicht l&ouml;schen",
+		"Sorry, cannot delete"		=> "Entschuldigung, kann nicht l&ouml;schen",
+		"Sorry, this file..."		=> "Entschuldigung, die Datei wurde ge&auml;ndert, kann nicht speichern",
+		"Reload this file..."		=> "Soll die Datei mit deiner neueren Version neu geladen werden?",
+		"There was a..."		=> "Irgend was ist schief gelaufen. ICEcoder l&auml;dt einfach noch mal neu.",
+		"displayed at"			=> "angezeigt bei",
+		"Enter filename to..."		=> "Dateinamen eingeben um es zu speichern",
+		"That file exists..."		=> "Die Datei existiert bereits, &uuml;berschreiben?",
+		"Saving"			=> "Speichere"
+	),
+
+	"get-branch" =>
+	array(
+		"There are no..."		=> "Es gibt keinen Unterschied zwischen der lokalen Datei und dem im GitHub Repo. Zur&uuml;ck zum regul&auml;ren Modus?",
+		"Sorry, there was..."		=> "Entschuldigung, es ist ein Fehler aufgetreten. Fehlercode:",
+		"Your local folder..."		=> "Dein lokales Verzeichnis ist leer, soll es geklont werden"
+	),
+
+	"github-manager" =>
+	array(
+		"Sorry, cannot create..."	=> "Entschuldigung, kann Verzeichnis nicht anlegen",
+		"Cannot update config..."	=> "Kannn die Config Datei nicht aktualisieren. Bitte Lese- und Schreibrechte setzen",
+		"and try again"			=> "und versuch es noch einmal",
+		"saving github paths"		=> "Speichere GitHub Pfad ...",
+		"github paths"			=> "GitHub Pfad",
+		"Choose existing path"		=> "W&auml;hle existierenden Pfad",
+		"Local path"			=> "Lokaler Pfad",
+		"Remote GitHub path"		=> "Ferner GitHub Pfad",
+		"Choose"			=> "Wh&auml;hle",
+		"Set local and..."		=> "L&ouml;sch lokalen und fernen Pfad",
+		"Update"			=> "Aktualisieren",
+		"Add new path"			=> "Neuen Pfad hinzuf&uuml;gen",
+		"Add"				=> "Hinfzuf&uuml;gen",
+		"Usage Info"			=> "Infos zur Verwendung:",
+		"Enter relative local..."	=> "Gib den relativen, lokalen Pfad (z.B. /server/meineDateien) und absoluten GitHub Pfad (z.B. https://github.com/user/repo oder https://github.com/user/repo/tree/branch f&uuml;r branches), wie im Beispiel an. Dadurch werden die entsprechenden Quellverzeichnisse als Paar angelegt.",
+		"You can then..."		=> "Du kannst ein Paar ausw&auml;hlenn, welches als neues Root Verzeichnis in ICECoder festgelegt wird.",
+		"The file manager..."		=> "Im Dateimanager erscheint ein GitHub Icon. Durch einen Klick auf das Icon, werden die Unterschiede zwischen beiden Versionen angezeigt. Diese Unterschiede können committed und zum GitHub Repo gepusht, oder zum lokale Pfad geklont werden.",
+		"If you want..."		=> "Soll ein weiterers Hauptverzeichnis gesetzt werden, kann dies in den Einstellungen vorgenommen werden."
+	),
+
+	"github" =>
+	array(
+		"Sorry, you do..."		=> "Es scheint so, als sei OpenSSL nicht mit PHP geladen worden, dadurch steht https nicht zur verf&uuml;gung. Dies wird jedoch f&uuml;r den Datentransfer zu GitHub ben&ouml;tigt. Bitte die PHP.ini entsprechend anpassen und neu starten."
+	),
+
+	"headers" =>
+	array(
+		"Bad CSRF token..."		=> "Fehlerhafter CSRF Token. Bitte den Fehler an https://github.com/mattpass/ICEcoder damit das Problem gel&ouml;st werden kann."
+	),
+
+	"help" =>
+	array(
+		"shortcuts"			=> "shortcuts",
+		"Within document"		=> "Innerhalb des Dokuments",
+		"On Tabs"			=> "Ge&ouml;ffnete Tabs",
+		"Within file manager"		=> "Innerhalb des Dateimanagers",
+		"Anywhere"			=> "&Uuml;berall",
+		"Space"				=> "Leerzeichen",
+		"Click"				=> "Klick",
+		"or"				=> "oder",
+		"Left click"			=> "Linksklick",
+		"Middle click"			=> "mittlerer Klick",
+		"Double click tap..."		=> "Doppelklick / Tap (Mobile)",
+		"Right click"			=> "Rechtsklick",
+		"Middle scrollwheel"		=> "mittleres Mausrad",
+		"Drag"				=> "Ziehen",
+		"Autocomplete add snippet"	=> "Autocomplete / snippet hinzuf&uuml;gen",
+		"Multiple select"		=> "Mehrfachauswahl",
+		"Move line up"			=> "Zeile h&ouml;her",
+		"Move line down"		=> "Zeile runter",
+		"Duplicate lines"		=> "Zeile(n) duplizieren",
+		"Remove lines"			=> "Zeile(n) entfernen",
+		"Insert line before"		=> "Zeile vorher einf&uuml;gen",
+		"Insert line after"		=> "Zeile danach ein&uuml;gen,
+		"Search for selected"		=> "Nach dem gesuchten ausw&auml;hlen",
+		"Jump to definition"		=> "Zur Definition springen / zur&uml;ck springen",
+		"Comment uncomment"		=> "Kommentieren / Auskommentieren",
+		"Insert tab indent"		=> "Tab einf&uuml;gen / Gedankenstrich selektiert",
+		"Wrap with div"			=> "Mit &lt;div&gt; umschlie&szlig;en",
+		"Wrap with span"		=> "Mit &lt;span&gt; umschlie&szlig;en",
+		"Wrap unwrap p"			=> "Umschlie&szlig;en / unwrap with &lt;p&gt;",
+		"Wrap unwrap a"			=> "Umschlie&szlig;en / Mit &lt;a&gt; auschlie&szlig;en",
+		"Wrap unwrap b"			=> "Umschlie&szlig;en / Mit &lt;b&gt; auschlie&szlig;en",
+		"Wrap unwrap i"			=> "Umschlie&szlig;en / Mit &lt;i&gt; auschlie&szlig;en",
+		"Wrap unwrap strong"		=> "Umschlie&szlig;en / Mit &lt;strong&gt; auschlie&szlig;en",
+		"Wrap unwrap em"		=> "Umschlie&szlig;en / Mit &lt;em&gt; auschlie&szlig;en",
+		"Wrap unwrap h1..."		=> "Umschlie&szlig;en / Mit &lt;h1&gt; - &lt;h3&gt; auschlie&szlig;en",
+		"End line with..."		=> "Zeile abschlie&szlig; mit &lt;br&gt;",
+		"Close tab"			=> "Tab schlie&szlig;en",
+		"Select file folder"		=> "Datei / Verzeichnis aus&auml;hlen",
+		"Open file"			=> "Datei &ouml;ffnen",
+		"Range select"			=> "Bereich ausw&auml;hlen",
+		"Options for selected"		=> "Optionen f&uuml;r ausgew&auml;hlte",
+		"Delete selected"		=> "Ausgew&auml;hlte l&ouml;schen",
+		"Next previous tab"		=> "N&auml;chster / Vorheriger Tab",
+		"Next tab"			=> "N&auml;chster Tab",
+		"Previous tab"			=> "Vorheriger Tab",
+		"New tab"			=> "Neuer Tab",
+		"Close current tab"		=> "Aktuellen Tab schlie&szlig;en",
+		"Open file prompt"		=> "Datei &ouml;ffnen Dialog anzeigen",
+		"Find"				=> "Finden",
+		"Focus on Go..."		=> "Fokus auf Gehe zu Zeile Feld setzen",
+		"Save"				=> "Speichern",
+		"Save as"			=> "Speichern als ...",
+		"View webpage"			=> "Webseite anzeigen",
+		"Contract expand file..."	=> "Alle Dateien / Verzeichnisse anzeigen",
+		"Fold unfold current..."	=> "Aktuelle Zeile einklappen",
+		"Refocus on document"		=> "Erneut den Fokus auf das Dokument setzen",
+		"Cancel tasks"			=> "Vorgang abbrechen"
+	),
+
+	"login" =>
+	array(
+		"set password"			=> "Passwort setzen",
+		"login"				=> "Login",
+		"To disable registration..."	=> "Um den Registrierungsmodus zu deaktivieren, &ouml;ffne im Men&uuml; die Einstellungen oder die Datein lib/config___settings.php und setze unter enableRegistration auf false, danach lade die Seite erneut.",
+		"Registration mode enabled"	=> "Registrierungsmodus aktiviert",
+		"auto-check for updates"	=> "Automatisch nach Update suchen",
+		"To put into..."		=> "Um den Mehrbenutzermodus zu aktivieren, &ouml;ffne im Men&uuml; die Einstellungen oder die Datein lib/config___settings.php und setze unter multiUser auf true, danach lade die Seite erneut",
+		"multi-user"			=> "Mehrbenutzermodus"
+	),
+
+	"multiple-results" =>
+	array(
+		"rename all"			=> "Alle umbenennen",
+		"replace all"			=> "Alle ersetzen
+		"document"			=> "Dokument",
+		"Found"				=> "Gefunden",
+		"times"				=> "",
+		"replace"			=> "ersetzen",
+		"file folder"			=> "Datei/Verzeichnis",
+		"rename to"			=> "Umbenennen nach",
+		"rename"			=> "Umbenennen",
+		"file"				=> "Datei",
+		"No matches found"		=> "Nichts gefunden",
+		"selected"			=> "Ausgew&auml;hlte",
+		"found in"			=> "Gefunden in",
+		"Replaced"			=> "Ersetzen"
+	)
+
+);
+?>
