34b34bcc885ec4be128ab979e9619b8c605a7145
lzh <lzh370@qq.com>
2014-08-22 15:42:28 +0800

    Updated Chinese translation

--- lang/chinese-simplified.php
+++ lang/chinese-simplified.php
@@ -109,7 +109,7 @@ $text = array(
 
 	"bug-files-check" =>
 	array(
-		"Found in"			=> "发现在:"
+		"Found in"			=> "搜索到:"
 	),
 
 	"file-control" =>
@@ -209,13 +209,14 @@ $text = array(
 		"Insert tab indent"		=> "插入tab / 插入选择",
 		"Wrap with div"			=> "封装 &lt;div&gt;",
 		"Wrap with span"		=> "封装 &lt;span&gt;",
-		"Wrap unwrap p"			=> "封装 / 撤销封装 with &lt;p&gt;",
-		"Wrap unwrap a"			=> "封装 / 撤销封装 with &lt;a&gt;",
-		"Wrap unwrap b"			=> "封装 / 撤销封装 with &lt;b&gt;",
-		"Wrap unwrap i"			=> "封装 / 撤销封装 with &lt;i&gt;",
-		"Wrap unwrap strong"		=> "封装 / 撤销封装 with &lt;strong&gt;",
-		"Wrap unwrap em"		=> "封装 / 撤销封装 with &lt;em&gt;",
-		"Wrap unwrap h1..."		=> "封装 / 撤销封装 with &lt;h1&gt; - &lt;h3&gt;",
+		"Wrap unwrap p"			=> "封装 / 撤销封装 &lt;p&gt;",
+		"Wrap unwrap a"			=> "封装 / 撤销封装 &lt;a&gt;",
+		"Wrap unwrap b"			=> "封装 / 撤销封装 &lt;b&gt;",
+		"Wrap unwrap i"			=> "封装 / 撤销封装 &lt;i&gt;",
+		"Wrap unwrap strong"		=> "封装 / 撤销封装 &lt;strong&gt;",
+		"Wrap unwrap em"		=> "封装 / 撤销封装 &lt;em&gt;",
+		"Wrap unwrap li"		=> "封装 / 撤销封装 &lt;li&gt;",
+		"Wrap unwrap h1..."		=> "封装 / 撤销封装 &lt;h1&gt; - &lt;h3&gt;",
 		"End line with..."		=> "换行 &lt;br&gt;",
 		"Close tab"			=> "关闭选项卡",
 		"Select file folder"		=> "选择文件 / 文件夹",
@@ -238,6 +239,34 @@ $text = array(
 		"Fold unfold current..."	=> "折叠 / 展开当前行",
 		"Refocus on document"		=> "光标重新聚焦于文档",
 		"Cancel tasks"			=> "取消任务"
+	),
+
+	"ice-coder" =>
+	array(
+		"No text selected..."		=> "搜索中没有选中的文本",
+		"Creating Folder"		=> "正在创建文件夹",
+		"Sorry you can..."		=> "抱歉, 只能同时打开100个文件!",
+		"Opening File"			=> "正在打开文件",
+		"Enter relative file..."	=> "输入本地相对路径 (前缀 /) 或远程 URL",
+		"Getting"			=> "正在获取",
+		"Please enter the..."		=> "请输入新的名称为",
+		"Renaming to"			=> "正在重命名",
+		"Moving to"			=> "正在移动到",
+		"Deleting File"			=> "正在删除文件",
+		"Pasting File"			=> "正在粘贴文件",
+		"Sorry cannot paste..."		=> "抱歉, 无法粘贴到根路径",
+		"Nothing to paste..."		=> "粘贴失败, 请先复制一个文件 / 文件夹!",
+		"Replacing text in"		=> "正在替换文本",
+		"Cancelled tasks"		=> "取消任务",
+		"Open previous files"		=> "打开以前的文件?",
+		"Please enter your..."		=> "请输入您的 GitHub token (允许使用个人令牌或客户端令牌). 可以查看 Github Auth Token 的帮助 > 更多信息在设置窗口",
+		"This will compare..."		=> "将比较和显示你的本地目录和 GitHub repo 之间的差异. 确定吗?",
+		"Please note for..."		=> "请注意: 需要更新才能正常工作, 你需要为所有 ICEcoder 文件和文件夹设置写入和删除权限. 如果您需要恢复 ICEcoder 到这个版本, 你可以在 /tmp 目录找到它们. 点击 OK 继续使用自动升级或点击 cancel 访问 ICEcoder 网站, 您也可以到 grab 的项目首页下载 zip 来进行手动更新.",
+		"You can start..."		=> "你可以在 帮助 > 设置中启动错误报告",
+		"Error cannot find..."		=> "错误: 无法找到/进入错误文件路径",
+		"No new errors..."		=> "没有新的错误被找到",
+		"You have made..."		=> "您已做的更改未保存。您确定要关闭它而不保存吗?",
+		"Close all tabs"		=> "是否关闭所有选项卡?"
 	),
 
 	"login" =>
@@ -267,6 +296,130 @@ $text = array(
 		"selected"			=> "选择",
 		"found in"			=> "查询结果在",
 		"Replaced"			=> "已替换"
+	),
+
+	"plugins-manager" =>
+	array(
+		"ICEcoder needs to..."		=> "ICEcoder 需要重新加载，这个插件才可以使用。现在刷新吗?",
+		"saving plugins"		=> "正在保存插件...",
+		"Cannot update config..."	=> "不能更新配置文件. 请为",
+		"and try again"			=> "设置写入权限并刷新重试",
+		"couldnt delete dir"		=> "不能删除目录",
+		"couldnt delete file"		=> "不能删除文件",
+		"plugins"			=> "插件",
+		"Guide to writing..."		=> "编写插件指南",
+		"Manage Installed"		=> "管理已安装",
+		"URL"				=> "URL",
+		"Target"			=> "目标",
+		"Timer"				=> "定时器",
+		"Update"			=> "更新",
+		"Install"			=> "安装",
+		"Uninstall"			=> "卸载",
+		"Reload after install..."	=> "安装后需要刷新"
+	),
+
+	"properties" =>
+	array(
+		"properties"			=> "属性",
+		"Size"				=> "尺寸",
+		"Modified"			=> "修改",
+		"Last access"			=> "最近访问",
+		"Type"				=> "类型",
+		"Readable Writeable"		=> "可读 / 可写",
+		"Relative path"			=> "相对路径",
+		"Absolute path"			=> "绝对路径",
+		"Contains"			=> "包含",
+		"Permissions"			=> "权限",
+		"Owner"				=> "所有者",
+		"Group"				=> "组",
+		"Public"			=> "公众",
+		"Read"				=> "读取",
+		"Write"				=> "写入",
+		"Execute"			=> "执行",
+		"Change to"			=> "更改为",
+		"update"			=> "更新"
+	),
+
+	"settings-common" =>
+	array(
+		"Your document does..."		=> "你的文档不是UTF-8编码, 它将被转换"
+	),
+
+	"settings-save-current-files" =>
+	array(
+		"Cannot update config..."	=> "不能更新配置文件. 请为",
+		"and try again"			=> "增加写入权限并再次尝试"
+	),
+
+	"settings-screen" =>
+	array(
+		"settings"			=> "设置",
+		"version"			=> "版本",
+		"website"			=> "网站",
+		"git"				=> "git",
+		"codemirror dir"		=> "codemirror 目录",
+		"codemirror version"		=> "codemirror 版本",
+		"file manager root"		=> "文件管理器的根目录ROOT",
+		"Free to use..."		=> "您可以自由使用它, 无论商业与否, 只需让我知道有任何很酷的使用或有定制的:)同样我们无任何责任和任何担保, 使用所有的责任是你的. 很多个人和公司为 ICEcoder 作出过贡献, 在此篇幅有限无法一一列举, 请访问贡献者详细列表",
+		"functionality"			=> "功能",
+		"check for updates..."		=> "启动时检查更新",
+		"auto open last..."		=> "启动时自动载入最后打开的文件",
+		"when finding in..."		=> "在查找文件时, 排除",
+		"assisting"			=> "辅助",
+		"code assist"			=> "代码辅助",
+		"visible tabs"			=> "缩进线可见",
+		"locked nav"			=> "锁定导航",
+		"tag wrapper command"		=> "标签封装命令",
+		"auto-complete on"		=> "打开自动完成",
+		"security"			=> "安全",
+		"new password"			=> "新密码",
+		"8 chars min"			=> "最少8个字符",
+		"confirm password"		=> "确认密码",
+		"banned files/folders"		=> "禁止文件 / 文件夹",
+		"banned paths"			=> "禁止路径",
+		"ip addresses"			=> "ip 地址",
+		"Slash prefixed comma..."	=> "斜线前缀, 以半角逗号分隔",
+		"Comma delimited"		=> "以半角逗号分隔",
+		"style"				=> "样式",
+		"theme"				=> "主题",
+		"line wrapping"			=> "换行",
+		"indent type"			=> "缩进类型",
+		"indent size"			=> "缩进大小",
+		"font size"			=> "字体大小",
+		"layout"			=> "布局",
+		"plugin panel aligned"		=> "插件板对齐",
+		"file manager"			=> "文件管理器",
+		"root"				=> "root",
+		"Slash prefixed"		=> "斜线前缀",
+		"bug reporting"			=> "错误报告",
+		"check in files"		=> "检查文件",
+		"every"				=> "任何",
+		"secs getting last"		=> "秒, 最后得到",
+		"lines"				=> "行",
+		"multi-user"			=> "多用户",
+		"Make sure you..."		=> "确保你没有把自己锁了",
+		"Registration"			=> "启用注册",
+		"auth token"			=> "auth token",
+		"Required to get..."		=> "如需要得到差异列表, 提交您 GitHub 上托管的 repo 等. 如果你还没有一个, 您可以使用:".PHP_EOL.PHP_EOL.
+							"- 个人访问令牌 (https://help.github.com/articles/creating-an-access-token-for-command-line-use), 或".PHP_EOL.
+							"- 完整的客户端/机密双令牌 (http://developer.github.com/v3/oauth).".PHP_EOL.PHP_EOL.
+							"我们不建议您在此处设置您的令牌, 更安全的方式是当ICEcoder要求输入时将其只保存在会话中.".PHP_EOL.PHP_EOL.
+							"然而, 如果你在一个值得信赖和安全的环境中工作, 把它设置在这里是更有效的.",
+		"Sorry cannot commit..."	=> "抱歉, 设置不能在演示模式下提交",
+		"update"			=> "更新"
+	),
+
+	"settings-update" =>
+	array(
+		"Cannot update config..."	=> "不能更新配置文件. 请为",
+		"and try again"			=> " 增加写入权限并重新尝试",
+		"and press refresh"		=> "增加写入权限并刷新"
+	),
+
+	"updater" =>
+	array(
+		"Update appears to..."		=> "更新似乎是成功的"
 	)
+	
 );
 ?>
