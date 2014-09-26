cc5cd166bebb01f231912e5c4c590b9f483cd159
Matt Pass <matt@mattpass.com>
2014-09-26 11:59:03 +0100

    Only if no headers sent, set the 3 headers

--- lib/headers.php
+++ lib/headers.php
@@ -25,9 +25,11 @@ if (($_GET || $_POST) && (!isset($_REQUEST["csrf"]) || $_REQUEST["csrf"] !== $_S
 		POST: ".xssClean(var_export($_POST, true),"html"));
 }
 
-// Set our security related headers
-header("X-Frame-Options: SAMEORIGIN");					// Only frames of same origin
-header("X-XSS-Protection: 1; mode=block");				// Turn on IE8-9 XSS prevention tools
-// header("X-Content-Security-Policy: allow 'self'");			// Only allows JS on same domain & not inline to run
-header("X-Content-Type-Options: nosniff");				// Prevent MIME based attacks
+if (!headers_sent()) {
+	// Set our security related headers
+	header("X-Frame-Options: SAMEORIGIN");					// Only frames of same origin
+	header("X-XSS-Protection: 1; mode=block");				// Turn on IE8-9 XSS prevention tools
+	// header("X-Content-Security-Policy: allow 'self'");			// Only allows JS on same domain & not inline to run
+	header("X-Content-Type-Options: nosniff");				// Prevent MIME based attacks
+}
 ?>
