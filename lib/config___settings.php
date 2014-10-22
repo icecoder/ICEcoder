8189985a7212b2359bb18d743b452e2a7e4e4739
Matt Pass <matt@mattpass.com>
2014-10-22 10:23:20 +0100

    Upgrade to CodeMirror 4.7

--- lib/config___settings.php
+++ lib/config___settings.php
@@ -2,7 +2,7 @@
 // ICEcoder system settings
 $ICEcoderSettings = array(
 	"versionNo"		=> "4.3",
-	"codeMirrorDir"		=> "CodeMirror-4.2",
+	"codeMirrorDir"		=> "CodeMirror-4.7",
 	"docRoot"		=> $_SERVER['DOCUMENT_ROOT'],
 	"demoMode"		=> false,
 	"devMode"		=> false,
