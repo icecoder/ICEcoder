9ea459787e305b618c5004b9f1b23b6a8a09f3eb
Matt Pass <matt@mattpass.com>
2014-10-24 09:23:52 +0100

    Polyfill added for array_replace_recursive
    This is natively available in PHP 5.3+

--- lib/settings-common.php
+++ lib/settings-common.php
@@ -128,4 +128,34 @@ function toUTF8noBOM($string,$message) {
 	}
 	return $string;
 }
+
+// Polyfill for array_replace_recursive, which is in PHP 5.3+
+if (!function_exists('array_replace_recursive')) {
+	function array_replace_recursive($base, $replacements) { 
+		foreach (array_slice(func_get_args(), 1) as $replacements) { 
+			$bref_stack = array(&$base); 
+			$head_stack = array($replacements); 
+
+			do { 
+				end($bref_stack); 
+
+				$bref = &$bref_stack[key($bref_stack)]; 
+				$head = array_pop($head_stack); 
+
+				unset($bref_stack[key($bref_stack)]); 
+
+				foreach (array_keys($head) as $key) { 
+					if (isset($key, $bref) && is_array($bref[$key]) && is_array($head[$key])) { 
+						$bref_stack[] = &$bref[$key]; 
+						$head_stack[] = $head[$key]; 
+					} else { 
+						$bref[$key] = $head[$key]; 
+					} 
+				} 
+			} while(count($head_stack)); 
+		} 
+
+		return $base;
+	}
+}
 ?>
