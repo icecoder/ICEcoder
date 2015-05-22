dcfd4f3ed2f55310fa6ca6f7aeb055fbccbde9be
Matt Pass <matt@mattpass.com>
2015-05-22 18:58:44 +0100

    System params file started
    Contains value in var to use to output to a file

---
+++ lib/settings-system-params.php
@@ -0,0 +1,18 @@
+$newConfigSettingsFile = '<?php
+// ICEcoder system settings
+$ICEcoderSettings = array(
+	"versionNo"		=> "5.0",
+	"codeMirrorDir"		=> "CodeMirror",
+	"docRoot"		=> $_SERVER[\'DOCUMENT_ROOT\'],	// Set absolute path of another location if needed
+	"demoMode"		=> false,
+	"devMode"		=> false,
+	"fileDirResOutput"	=> "none",			// Can be none, raw, object, both (all but \'none\' output to console)
+	"loginRequired"		=> true,
+	"multiUser"		=> false,
+	"languageBase"		=> "english.php",
+	"lineEnding"		=> "\n",
+	"newDirPerms"		=> 755,
+	"newFilePerms"		=> 644,
+	"enableRegistration"	=> true
+);
+?>';
