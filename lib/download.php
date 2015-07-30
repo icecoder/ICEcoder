e741d6c207668b307e5d48565224a2287ea5e51f
Matt Pass <matt@mattpass.com>
2015-07-30 19:16:05 +0100

    Cache params added and ensuring app/octet-stream

--- lib/download.php
+++ lib/download.php
@@ -10,8 +10,14 @@ if (!file_exists($file) || strpos(str_replace("\\","/",$file),$docRoot) !== 0) {
 }
 
 if (file_exists($file)) {
+    header("Pragma: public");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Cache-Control: public");
     header('Content-Description: File Transfer');
+    header("Content-Type: application/octet-stream");
     header('Content-Disposition: attachment; filename='.basename($file));
+    // header("Content-Transfer-Encoding: binary");
     header('Content-Length: '.filesize($file));
     ob_clean();
     flush();
