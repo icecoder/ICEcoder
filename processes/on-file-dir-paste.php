883d4d20efbea6abbf5ac64adc074be3d602c80d
Matt Pass <matt@mattpass.com>
2019-09-21 23:37:15 +0100

    JS paste process example added

--- processes/on-file-dir-paste.php
+++ processes/on-file-dir-paste.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when a file or dir is pasted, has $fileLoc, $fileName and $dest strings available to it
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../file-dir-access.log", 'a');
 // fwrite($fh, "PASTE >>> ".date("D dS M Y h:i:sa").": ".$dest."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('pasted');";
+$doNext .= "";
