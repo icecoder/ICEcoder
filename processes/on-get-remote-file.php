b8d145fd04a0219eb382503a651881ae2fb4fd6d
Matt Pass <matt@mattpass.com>
2019-09-21 23:40:41 +0100

    JS got remote file process example added

--- processes/on-get-remote-file.php
+++ processes/on-get-remote-file.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when a remote file is loaded, has $file string available to it
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../file-dir-access.log", 'a');
 // fwrite($fh, "GET REMOTE FILE >>> ".date("D dS M Y h:i:sa").": ".$file."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('got remote file');";
+$doNext .= "";
