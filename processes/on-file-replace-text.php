e2ce0a0f14eed27738f21832c2aa8bd83174f94a
Matt Pass <matt@mattpass.com>
2019-09-21 23:39:14 +0100

    JS replace text process example added

--- processes/on-file-replace-text.php
+++ processes/on-file-replace-text.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when a file has text in it replaced, has $_GET['fileRef'] string available to it
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../file-dir-access.log", 'a');
 // fwrite($fh, "REPLACED TEXT >>> ".date("D dS M Y h:i:sa").": ".$_GET['fileRef']."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('renamed');";
+$doNext .= "";
