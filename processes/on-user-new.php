0519cde4df0e883b19af0bf17d8d73025efeb800
Matt Pass <matt@mattpass.com>
2019-09-21 23:42:50 +0100

    JS new user process example added

--- processes/on-user-new.php
+++ processes/on-user-new.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when a new user sets up
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../user-access.log", 'a');
 // fwrite($fh, "NEW >>> ".date("D dS M Y h:i:sa").": ".$_SESSION['username']."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('new user');";
+$doNext .= "";
