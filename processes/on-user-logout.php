f25b5f225e14607fcdc5c02e5e38810163ade478
Matt Pass <matt@mattpass.com>
2019-09-21 23:42:28 +0100

    JS user logout process example added

--- processes/on-user-logout.php
+++ processes/on-user-logout.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when a user logs out
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../user-access.log", 'a');
 // fwrite($fh, "logout  ".date("D dS M Y h:i:sa").": ".$_SESSION['username']."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('logout');";
+$doNext .= "";
