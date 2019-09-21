e656082ef3dd335110b0a7e09f0ddbc6a78c694c
Matt Pass <matt@mattpass.com>
2019-09-21 23:38:21 +0100

    JS renamed process example added

--- processes/on-file-dir-rename.php
+++ processes/on-file-dir-rename.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when a file or dir is renamed, has $fileLoc and $fileName strings available to it
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../file-dir-access.log", 'a');
 // fwrite($fh, "RENAMED >>> ".date("D dS M Y h:i:sa").": ".$fileLoc."/".$fileName."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('renamed');";
+$doNext .= "";
