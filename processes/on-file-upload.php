7a2bcf1d25bb3d8d68f57b5427c5949b8f1f2ac8
Matt Pass <matt@mattpass.com>
2019-09-21 23:40:11 +0100

    JS uploaded process example added

--- processes/on-file-upload.php
+++ processes/on-file-upload.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when files are uploaded, has $uploads array available to it
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../file-dir-access.log", 'a');
 // fwrite($fh, "UPLOAD >>> ".date("D dS M Y h:i:sa").": ".($uploads[0]->name)."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('uploaded');";
+$doNext .= "";
