efb454cb2d9e43b48705599e5906511f087a86fc
Matt Pass <matt@mattpass.com>
2019-09-21 23:37:49 +0100

    JS perms change process example added

--- processes/on-file-dir-perms.php
+++ processes/on-file-dir-perms.php
@@ -3,8 +3,11 @@ if (!isset($_SESSION['loggedIn'])) {
 	die('Sorry, not logged in.');
 }
 // Purpose:	This file is run when a file or dir has its perms changed, has $fileLoc, $fileName and $_GET['perms'] strings available to it
-// Langs:	PHP only
+// Langs:	PHP (tho can concat JS within $doNext string, see below)
 // Example:
 // $fh = fopen(dirname(__FILE__)."/../file-dir-access.log", 'a');
 // fwrite($fh, "PERMS >>> ".date("D dS M Y h:i:sa").": ".$fileLoc."/".$fileName." = ".$_GET['perms']."\n");
 // fclose($fh);
+
+// If JS is needed, add within $doNext string below, eg $doNext .= ";alert('perms changed');";
+$doNext .= "";
