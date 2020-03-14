80447a518267815d87fc7b8ca3d83458784b7766
mattpass <matt@helical-levity.com>
2020-03-14 07:39:19 +0000

    Remove GitHub items from files.php, tidy up

--- files.php
+++ files.php
@@ -2,52 +2,37 @@
 include("lib/headers.php");
 include("lib/settings.php");
 $t = $text['files'];
-
-// Is our dir in the list of GitHub local paths?
-$isGitHubRepoDir = in_array($ICEcoder["root"],$ICEcoder['githubLocalPaths']);
 ?>
 <!DOCTYPE html>
 
 <html onMouseDown="parent.ICEcoder.mouseDown=true; parent.ICEcoder.resetAutoLogoutTimer(); parent.ICEcoder.boxSelect(event,'down')" onMouseUp="parent.ICEcoder.mouseDown=false; parent.ICEcoder.resetAutoLogoutTimer(); parent.ICEcoder.mouseDownInCM=false; parent.ICEcoder.boxSelect(event,'up'); if (!parent.ICEcoder.overCloseLink) {parent.ICEcoder.tabDragEnd()}" onMouseMove="if(parent.ICEcoder) {parent.ICEcoder.getMouseXY(event,'files'); parent.ICEcoder.resetAutoLogoutTimer(); parent.ICEcoder.canResizeFilesW(); parent.ICEcoder.boxSelect(event,'drag')}" onDrop="if(parent.ICEcoder) {parent.ICEcoder.getMouseXY(event,'files')}" onContextMenu="parent.ICEcoder.selectFileFolder(event); return parent.ICEcoder.showMenu(event)" onClick="if (!parent.ICEcoder.fmDraggedBox) {parent.ICEcoder.selectFileFolder(event)} else {parent.ICEcoder.fmDraggedBox = false}" onDragStart="parent.ICEcoder.selectFileFolder(event);" onDragOver="event.preventDefault();event.stopPropagation()">
 <head>
-<title>ICEcoder v <?php echo $ICEcoder["versionNo"];?> file manager</title>
-<meta name="robots" content="noindex, nofollow">
-<link rel="stylesheet" type="text/css" href="lib/files.css?microtime=<?php echo microtime(true);?>">
-<link rel="stylesheet" type="text/css" href="lib/file-types.css?microtime=<?php echo microtime(true);?>">
-<link rel="stylesheet" type="text/css" href="lib/file-type-icons.css?microtime=<?php echo microtime(true);?>">
-<script src="lib/ice-coder<?php if (!$ICEcoder['devMode']) {echo '.min';}; echo ".js?microtime=".microtime(true);?>" type="text/javascript"></script>
-<!--Updated via settings so must remain 4th stylesheet//-->
-<style>
-    ul.fileManager li a span { font-size:  <?php echo $ICEcoder["fontSize"];?>; }
-</style>
+    <title>ICEcoder v <?php echo $ICEcoder["versionNo"];?> file manager</title>
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="stylesheet" type="text/css" href="lib/files.css?microtime=<?php echo microtime(true);?>">
+    <link rel="stylesheet" type="text/css" href="lib/file-types.css?microtime=<?php echo microtime(true);?>">
+    <link rel="stylesheet" type="text/css" href="lib/file-type-icons.css?microtime=<?php echo microtime(true);?>">
+    <script src="lib/ice-coder<?php if (!$ICEcoder['devMode']) {echo '.min';}; echo ".js?microtime=".microtime(true);?>" type="text/javascript"></script>
+    <!--Updated via settings so must remain 4th stylesheet//-->
+    <style>
+        ul.fileManager li a span { font-size:  <?php echo $ICEcoder["fontSize"];?>; }
+    </style>
 </head>
 
-<body onFocus="parent.ICEcoder.files.style.background='#444'" onBlur="parent.ICEcoder.files.style.background='#383838'" onload="parent.ICEcoder.showHideGithubNav(parent.ICEcoder.githubDiff ? 'show' : 'hide')" onDblClick="parent.ICEcoder.openFile()" onKeyDown="return parent.ICEcoder.interceptKeys('files', event);" onKeyUp="parent.ICEcoder.resetKeys(event);" onBlur="parent.ICEcoder.resetKeys(event);">
+<body onFocus="parent.ICEcoder.files.style.background='#444'" onBlur="parent.ICEcoder.files.style.background='#383838'" onDblClick="parent.ICEcoder.openFile()" onKeyDown="return parent.ICEcoder.interceptKeys('files', event);" onKeyUp="parent.ICEcoder.resetKeys(event);" onBlur="parent.ICEcoder.resetKeys(event);">
 
 <div title="<?php echo $t['Lock'];?>" onClick="parent.ICEcoder.lockUnlockNav()" id="fmLock" class="lock"></div>
 <div title="<?php echo $t['Refresh'];?>" onClick="parent.ICEcoder.refreshFileManager()" class="refresh"></div>
 <div title="<?php echo $t['Plugins'];?>" onClick="parent.ICEcoder.showHidePlugins(parent.get('plugins').style.width != '55px' ? 'show' : 'hide')" class="plugins"></div>
-<?php
-$_SESSION['githubDiff'] = false;
-if ($isGitHubRepoDir) {
-	$classExtra = !isset($_GET["githubDiff"]) || $_GET["githubDiff"] == "false" ? "Off" : "On";
-	if ($classExtra == "On") {
-		$_SESSION['githubDiff'] = true;
-		// Make sure we are showing the diff pane
-		echo "<script>parent.ICEcoder.setSplitPane('on');</script>";
-	}
-	echo '<div title="GitHub" onClick="parent.ICEcoder.githubDiffToggle()" class="github'.$classExtra.'"></div>';
-}
-?>
 
 <ul class="fileManager">
-<li class="pft-directory dirOpen"><a nohref title="/" onMouseOver="parent.ICEcoder.overFileFolder('folder','/')" onMouseOut="parent.ICEcoder.overFileFolder('folder','')" onClick="parent.ICEcoder.openCloseDir(this)" style="position: relative; left:-22px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span id="|">/ <?php
-echo $iceRoot == "" ? $t['ROOT'] : trim($iceRoot,"/");
-$thisPermVal = $serverType=="Linux" ? substr(sprintf('%o', fileperms($docRoot.$iceRoot)), -3) : "";
-$permColors = $thisPermVal == 777 ? 'background: #800; color: #eee' : 'color: #888';
-?></span> <span style="<?php echo $permColors;?>; font-size: 8px" id="|_perms"><?php echo $thisPermVal;;?></span></a></li><?php
-// tree file items generated by the iFrame 'fileControl' below which loads in the items at location=| (ie, the root)
-?>
+    <li class="pft-directory dirOpen"><a nohref title="/" onMouseOver="parent.ICEcoder.overFileFolder('folder','/')" onMouseOut="parent.ICEcoder.overFileFolder('folder','')" onClick="parent.ICEcoder.openCloseDir(this)" style="position: relative; left:-22px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span id="|">/ <?php
+                echo $iceRoot == "" ? $t['ROOT'] : trim($iceRoot,"/");
+                $thisPermVal = $serverType=="Linux" ? substr(sprintf('%o', fileperms($docRoot.$iceRoot)), -3) : "";
+                $permColors = $thisPermVal == 777 ? 'background: #800; color: #eee' : 'color: #888';
+                ?></span> <span style="<?php echo $permColors;?>; font-size: 8px" id="|_perms"><?php echo $thisPermVal;;?></span></a></li><?php
+    // tree file items generated by the iFrame 'fileControl' below which loads in the items at location=| (ie, the root)
+    ?>
 </ul>
 
 <iframe name="fileControl" src="lib/get-branch.php?location=|&csrf=<?php echo $_SESSION['csrf'];?>" style="display: none"></iframe>
