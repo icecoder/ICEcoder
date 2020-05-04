b0c0d86b7ca3cbf90616f06df202eff51bc3e6e1
mattpass <matt@helical-levity.com>
2020-05-04 07:33:37 +0100

    download.php updates

--- lib/download.php
+++ lib/download.php
@@ -3,22 +3,22 @@ include("headers.php");
 include("settings.php");
 
 // Establish the real absolute path to the file
-$file = realpath($docRoot.$iceRoot.str_replace("|","/",$_GET['file']));
+$file = realpath($docRoot . $iceRoot . str_replace("|", "/", $_GET['file']));
 // If it doesn't exist, or doesn't start with the $docRoot, stop here
-if (!file_exists($file) || strpos(str_replace("\\","/",$file),$docRoot) !== 0) {
+if (false === file_exists($file) || 0 !== strpos(str_replace("\\", "/", $file), $docRoot)) {
 	die("<script>ICEcoder.message('Sorry, that file doesn\'t appear to exist');</script>");
 }
 
-if (file_exists($file)) {
+if (true === file_exists($file)) {
     header("Pragma: public");
     header("Expires: 0");
     header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
     header("Cache-Control: public");
     header('Content-Description: File Transfer');
     header("Content-Type: application/octet-stream");
-    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Disposition: attachment; filename=' . basename($file));
     // header("Content-Transfer-Encoding: binary");
-    header('Content-Length: '.filesize($file));
+    header('Content-Length: ' . filesize($file));
     ob_clean();
     flush();
     readfile($file);
