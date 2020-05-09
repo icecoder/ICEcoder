d0b093d7d52376ece824ed23ba9058dd86665361
mattpass <matt@helical-levity.com>
2020-05-09 13:19:39 +0100

    icecoder.php added as master loader

---
+++ lib/icecoder.php
@@ -0,0 +1,11 @@
+<?php
+// Classes
+require_once "../classes/_ExtraProcesses.php";
+require_once "../classes/System.php";
+
+// Headers & Settings
+require_once "headers.php";
+require_once "settings.php";
+
+// FTP Control
+require_once "ftp-control.php";
