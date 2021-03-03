edaa95df939030af32ab1698bbdba023b8d47f5a
mattpass <matt@helical-levity.com>
2021-03-03 13:29:05 +0000

    Fix file to use CSS file

--- lib/database-adminer-480-en.php
+++ lib/database-adminer-480-en.php
@@ -1140,7 +1140,7 @@ csp(){return
 csp();}function
 head(){return
 true;}function
-css(){$I=array();$p="adminer.css";if(file_exists($p))$I[]="$p?v=".crc32(file_get_contents($p));return$I;}function
+css(){$I=array();$p="../assets/css/database.css";if(file_exists($p))$I[]="$p?v=".crc32(file_get_contents($p));return$I;}function
 loginForm(){global$hc;echo"<table cellspacing='0' class='layout'>\n",$this->loginFormField('driver','<tr><th>'.'System'.'<td>',html_select("auth[driver]",$hc,DRIVER,"loginDriver(this);")."\n"),$this->loginFormField('server','<tr><th>'.'Server'.'<td>','<input name="auth[server]" value="'.h(SERVER).'" title="hostname[:port]" placeholder="localhost" autocapitalize="off">'."\n"),$this->loginFormField('username','<tr><th>'.'Username'.'<td>','<input name="auth[username]" id="username" value="'.h($_GET["username"]).'" autocomplete="username" autocapitalize="off">'.script("focus(qs('#username')); qs('#username').form['auth[driver]'].onchange();")),$this->loginFormField('password','<tr><th>'.'Password'.'<td>','<input type="password" name="auth[password]" autocomplete="current-password">'."\n"),$this->loginFormField('db','<tr><th>'.'Database'.'<td>','<input name="auth[db]" value="'.h($_GET["db"]).'" autocapitalize="off">'."\n"),"</table>\n","<p><input type='submit' value='".'Login'."'>\n",checkbox("auth[permanent]",1,$_COOKIE["adminer_permanent"],'Permanent login')."\n";}function
 loginFormField($D,$yd,$Y){return$yd.$Y;}function
 login($we,$F){if($F=="")return
