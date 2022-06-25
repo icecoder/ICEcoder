4ac0f0b43a6ad958c2bf257ef8b47e4038b8e467
mattpass <matt@helical-levity.com>
2022-06-25 20:46:17 +0100

    Changing REQUEST to POST and GET

--- lib/headers.php
+++ lib/headers.php
@@ -18,15 +18,17 @@ if (!isset($_SESSION["csrf"])) {
 	$_SESSION["csrf"] = md5(uniqid(mt_rand(), true));
 }
 
-if (($_GET || $_POST) && (!isset($_REQUEST["csrf"]) || $_REQUEST["csrf"] !== $_SESSION["csrf"])) {
-	$req = isset($_REQUEST["csrf"]) ? xssClean($_REQUEST["csrf"], "html") : "";
-	die($t['Bad CSRF token...'] . "<br><br>
-		CSRF issue:<br>
-		REQUEST: " . $req . "<br>
-		SESSION: " . xssClean($_SESSION["csrf"], "html") . "<br>
-		FILE: " . xssClean($_SERVER["SCRIPT_NAME"], "html") . "<br>
-		GET: " . xssClean(var_export($_GET, true), "html") . "<br>
-		POST: " . xssClean(var_export($_POST, true), "html"));
+if (($_POST || $_GET) && !$_POST["csrf"] && !$_GET["csrf"]) {
+	$req = xssClean($_POST["csrf"] ?? $_GET['csrf'] ?? "", "html");
+    if ($req !== $_SESSION["csrf"]) {
+        die($t['Bad CSRF token...'] . "<br><br>
+            CSRF issue:<br>
+            REQUEST: " . $req . "<br>
+            SESSION: " . xssClean($_SESSION["csrf"], "html") . "<br>
+            FILE: " . xssClean($_SERVER["SCRIPT_NAME"], "html") . "<br>
+            GET: " . xssClean(var_export($_GET, true), "html") . "<br>
+            POST: " . xssClean(var_export($_POST, true), "html"));
+    }
 }
 
 if (!headers_sent()) {
